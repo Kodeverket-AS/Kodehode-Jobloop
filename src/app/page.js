87a2725cf59b1777fa79e54360cda12aa5b182ca
RasmusKodehode <rasmus.kodehode@gmail.com>
2023-10-09 10:34:22 +0200

    added empty components

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+    <main className="flex flex-col items-center min-h-screen p-24">
       <p>Denne siden er under oppbygging!</p>
       <p>Dette blir Kodehodes nye nettside på kodehode.no</p>
     </main>
