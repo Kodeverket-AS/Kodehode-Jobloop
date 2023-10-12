0b03c61ed665a707c890916f62b713ae059ac6aa
RasmusKodehode <rasmus.kodehode@gmail.com>
2023-10-12 11:15:12 +0200

    empty subpages added

---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <main className="flex flex-col items-center min-h-screen p-24">
+      <p>Denne siden er under oppbygging!</p>
+      <p>Dette blir Om oss-siden til Kodehode</p>
+    </main>
+  );
+}
---
+++ src/app/virksomhet/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Virksomhet() {
+  return (
+    <main className="flex flex-col items-center min-h-screen p-24">
+      <p>Denne siden er under oppbygging!</p>
+      <p>Dette blir Viksomhetssiden til Kodehode</p>
+    </main>
+  );
+}
