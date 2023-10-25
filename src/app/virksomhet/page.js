1d9042c503ff673c62110e6a39a481bc356c3f8a
RasmusKodehode <rasmus.kodehode@gmail.com>
2023-10-25 14:36:19 +0200

    hero keys added

--- src/app/virksomhet/page.js
+++ src/app/virksomhet/page.js
@@ -6,7 +6,7 @@ export default async function Virksomhet() {
   const data = await getData();
   return (
     <main className="flex flex-col items-center min-h-screen p-24">
-      <HeroSub content={data.herosub[2]} />
+      <HeroSub content={data.herosub[2]} key={data.herosub[2]._id} />
       <p>Denne siden er under oppbygging!</p>
       <p>Dette blir Viksomhetssiden til Kodehode</p>
     </main>
