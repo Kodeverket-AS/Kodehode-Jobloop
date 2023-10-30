ac6724512789d90f534ed302627ecc2b7ba758ec
RasmusKodehode <rasmus.kodehode@gmail.com>
2023-10-30 15:08:27 +0100

    tailwind configuration components

--- src/app/visjon/page.js
+++ src/app/visjon/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Vision() {
   return (
     <main className="flex flex-col items-center min-h-screen p-24">
-      <p>Denne siden er under oppbygging!</p>
+      <p className="font-bold bg-red-700">Denne siden er under oppbygging!</p>
       <p>Her kommer Vår Visjon hos Kodehode</p>
     </main>
   );
