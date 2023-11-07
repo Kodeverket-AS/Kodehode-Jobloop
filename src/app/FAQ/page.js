88d5044d63f64a0e7b5e8459fef5bb54c617c1f2
RasmusKodehode <rasmus.kodehode@gmail.com>
2023-11-07 14:51:25 +0100

    redistribution of Navbar

--- src/app/FAQ/page.js
+++ src/app/FAQ/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import { NavbarAlt } from "../../../components/navigation/Navbar";
 
 export default function FAQ() {
   return (
-    <main className="flex flex-col items-center min-h-screen p-24">
-      <p className="font-bold">Denne siden er under oppbygging!</p>
-      <p>Her kommer noen spørsmål og svar hos Kodehode</p>
-    </main>
+    <>
+      <NavbarAlt />
+      <main className="flex flex-col items-center min-h-screen p-24">
+        <p className="font-bold">Denne siden er under oppbygging!</p>
+        <p>Her kommer noen spørsmål og svar hos Kodehode</p>
+      </main>
+    </>
   );
 }
--- src/app/virksomhet/page.js
+++ src/app/virksomhet/page.js
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import HeroSub from "../../../components/HeroSub";
 import { getData } from "../page";
+import { NavbarAlt } from "../../../components/navigation/Navbar";
 
 export default async function Virksomhet() {
   const data = await getData();
   return (
-    <main className="flex flex-col items-center min-h-screen p-24">
-      <HeroSub content={data.heroSub[1]} key={data.heroSub[1]._id} />
-      <p>Denne siden er under oppbygging!</p>
-      <p>Dette blir Viksomhetssiden til Kodehode</p>
-    </main>
+    <>
+      <NavbarAlt />
+      <main className="flex flex-col items-center min-h-screen p-24">
+        <HeroSub content={data.heroSub[1]} key={data.heroSub[1]._id} />
+        <p>Denne siden er under oppbygging!</p>
+        <p>Dette blir Viksomhetssiden til Kodehode</p>
+      </main>
+    </>
   );
 }
