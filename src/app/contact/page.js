b51ed3f2a41136ac8793678cc2b1175b5f540509
RasmusKodehode <rasmus.kodehode@gmail.com>
2023-11-07 14:51:25 +0100

    redistribution of Navbar

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import { NavbarAlt } from "../../../components/navigation/Navbar";
 
 export default function Contact() {
   return (
-    <main className="flex flex-col items-center min-h-screen p-24">
-      <p>Denne siden er under oppbygging!</p>
-      <p>Her kommer et kontaktskjema hos Kodehode</p>
-    </main>
+    <>
+      <NavbarAlt />
+      <main className="flex flex-col items-center min-h-screen p-24">
+        <p>Denne siden er under oppbygging!</p>
+        <p>Her kommer et kontaktskjema hos Kodehode</p>
+      </main>
+    </>
   );
 }
