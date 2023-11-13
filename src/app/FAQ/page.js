76236c1d2038a8cda3bdcc2039ca5651784b7286
RasmusKodehode <rasmus.kodehode@gmail.com>
2023-11-13 09:16:21 +0100

    empty string on secondary subpages

--- src/app/FAQ/page.js
+++ src/app/FAQ/page.js
@@ -4,7 +4,7 @@ import { NavbarAlt } from "../../../components/navigation/Navbar";
 export default function FAQ() {
   return (
     <>
-      <NavbarAlt />
+      <NavbarAlt subpage="" />
       <main className="flex flex-col items-center min-h-screen p-24">
         <p className="font-bold">Denne siden er under oppbygging!</p>
         <p>Her kommer noen spørsmål og svar hos Kodehode</p>
--- src/app/visjon/page.js
+++ src/app/visjon/page.js
@@ -4,7 +4,7 @@ import { NavbarAlt } from "../../../components/navigation/Navbar";
 export default function Vision() {
   return (
     <>
-      <NavbarAlt />
+      <NavbarAlt subpage="" />
       <main className="flex flex-col items-center min-h-screen p-24">
         <p className="font-bold bg-red-700">Denne siden er under oppbygging!</p>
         <p>Her kommer Vår Visjon hos Kodehode</p>
