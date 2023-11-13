aea27b3b7f0616f018a5ef33fda39ffe16a6d962
RasmusKodehode <rasmus.kodehode@gmail.com>
2023-11-13 09:51:54 +0100

    fixed issue with sanity array

--- src/app/bedrift/page.js
+++ src/app/bedrift/page.js
@@ -5,12 +5,12 @@ import { NavbarAlt } from "../../../components/navigation/Navbar";
 
 export default async function Bedrift() {
   const data = await getData();
-  console.log(data.heroSub[2])
+  console.log(data.heroSub[3])
   return (
     <>
       <NavbarAlt subpage="Bedrift" />
       <main className="flex flex-col items-center min-h-screen p-24">
-        <HeroSub content={data.heroSub[2]} key={data.heroSub[2]._id} />
+        <HeroSub content={data.heroSub[3]} key={data.heroSub[3]._id} />
         <p>Denne siden er under oppbygging!</p>
         <p>Dette blir bedriftssiden til Kodehode</p>
       </main>
