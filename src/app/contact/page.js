12329ec9aadcad08565fb7897c0af1cfd5eb089e
RasmusKodehode <rasmus.kodehode@gmail.com>
2023-11-13 09:16:21 +0100

    empty string on secondary subpages

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,7 +4,7 @@ import { NavbarAlt } from "../../../components/navigation/Navbar";
 export default function Contact() {
   return (
     <>
-      <NavbarAlt />
+      <NavbarAlt subpage="" />
       <main className="flex flex-col items-center min-h-screen p-24">
         <p>Denne siden er under oppbygging!</p>
         <p>Her kommer et kontaktskjema hos Kodehode</p>
--- src/app/techstack/page.js
+++ src/app/techstack/page.js
@@ -4,7 +4,7 @@ import { NavbarAlt } from "../../../components/navigation/Navbar";
 export default function Techstack() {
   return (
     <>
-      <NavbarAlt />
+      <NavbarAlt subpage="" />
       <main className="flex flex-col items-center min-h-screen p-24">
         <p className="font-bold">Denne siden er under oppbygging!</p>
         <p>Her kommer techstacken hos Kodehode</p>
