a4dd77c101e2b78b28f12bdc5580aceb7188e6c1
RasmusKodehode <rasmus.kodehode@gmail.com>
2023-11-13 09:22:34 +0100

    fixed empty string in subpage

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,7 +4,7 @@ import { NavbarAlt } from "../../../components/navigation/Navbar";
 export default function Contact() {
   return (
     <>
-      <NavbarAlt subpage="" />
+      <NavbarAlt />
       <main className="flex flex-col items-center min-h-screen p-24">
         <p>Denne siden er under oppbygging!</p>
         <p>Her kommer et kontaktskjema hos Kodehode</p>
