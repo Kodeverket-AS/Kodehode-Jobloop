4424506f302ef4240c8a2d8c786e3f48e102831b
RasmusKodehode <rasmus.kodehode@gmail.com>
2023-11-22 14:05:45 +0100

    function to display cards

--- src/app/virksomhet/page.js
+++ src/app/virksomhet/page.js
@@ -3,6 +3,7 @@ import HeroSub from "../../../components/HeroSub";
 import EgenskapCoop from "../../../components/EgenskapCoop";
 import { getData } from "../page";
 import Navbar, { NavbarAlt } from "../../../components/navigation/Navbar";
+import KodehodeSummary from "../../../components/KodehodeSummary";
 
 export default async function Virksomhet() {
   const data = await getData();
@@ -18,6 +19,7 @@ export default async function Virksomhet() {
       <main className="flex flex-col items-center min-h-screen p-24">
         <HeroSub content={data.heroSub[2]} key={data.heroSub[2]._id} />
         <div className="flex flex-col items-center mt-80 md:mt-0">
+          <KodehodeSummary content1={data.origin} content2={data.courseTypes} />
           <p>Denne siden er under oppbygging!</p>
           <p>Dette blir Viksomhetssiden til Kodehode</p>
         </div>
