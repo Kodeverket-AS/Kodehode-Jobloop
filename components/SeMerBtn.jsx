55ac1a05f67bcb2d0fcc773f36b31aa74b8fc745
Anders Hausvik <andykodehode@gmail.com>
2023-12-09 12:36:29 +0100

    landingsside endringer

--- components/SeMerBtn.jsx
+++ components/SeMerBtn.jsx
@@ -9,7 +9,7 @@ export function SeMerBtn(){
     return(
         
             <button 
-            className="rounded-3xl w-30 h-12 border-solid border-black border-2 bg-white transition-all duration-600  hover:bg-alt-purple hover:border-none hover:text-white"
+            className="rounded-3xl w-32 h-10 border-solid border-black border-2 bg-white transition-all duration-600  hover:bg-alt-purple hover:border-none hover:text-white"
         
             >
                 Se Mer
--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,14 @@ import { Client } from '../../lib/sanity';
 import Image from 'next/image';
 import "./globals.css";
 import LandingHero from '../../components/LandingHero';
-import anniespratt from '../../public/anniespratt.jpg'
 import danialigdery from '../../public/danialigdery.jpg'
 import { ContactBtn } from '../../components/ContactBtn';
-import briefcase from '../../public/briefcase.png'
-import cap from '../../public/cap.png'
-import folderkanban from '../../public/folderkanban.png'
 import mappe from '../../public/mappe.png'
+import koffert2 from '../../public/koffert2.png'
+import hatt from '../../public/hatt.png'
+import { SeMerBtn } from '../../components/SeMerBtn';
+import visjonlanding2 from '../../public/visjonlanding2.jpeg'
+
 
 
 
@@ -18,45 +19,77 @@ export default function Home() {
       <div>
       <LandingHero></LandingHero>
       </div>
-      <div className='w-screen flex justify-center '>
-        <div className='flex flex-row justify-center gap-x-10 w-5/6' >
-          <div className='w-3/6 flex flex-col gap-20'>
-            <div>
-              <p>Vi rekrutterer kandidater som av ulike grunner har havnet utenfor arbeidslivet og lærer dem opp til å bli frontend utviklere. 
+      <div className='flex flex-col gap-32'>
+          <div className='w-screen flex justify-center '>
+            <div className='flex flex-row justify-center gap-x-10 w-5/6' >
+              <div className='w-3/6 flex flex-col gap-20'>
+                <div>
+                  <p>Vi rekrutterer kandidater som av ulike grunner har havnet utenfor arbeidslivet og lærer dem opp til å bli frontend utviklere. 
 
-              Gjennom en kombinasjon av teoretisk opplæring og praksis vil kandidatene lære seg de viktigste teknologiene og bli klar for en karriere innen utvikling. 
+                  Gjennom en kombinasjon av teoretisk opplæring og praksis vil kandidatene lære seg de viktigste teknologiene og bli klar for en karriere innen utvikling. 
 
-              Vi vil gjerne høre fra deg, enten du vil bli et kodehode, eller om du er en bedrift som vil samarbeide med oss og hjelpe våre kandidater, og om du er fra NAV eller andre tiltak og er nysgjerrig på vår virksomhet
-              </p>
-            </div>
-            <div>
-                <ContactBtn></ContactBtn>
+                  Vi vil gjerne høre fra deg, enten du vil bli et kodehode, eller om du er en bedrift som vil samarbeide med oss og hjelpe våre kandidater, og om du er fra NAV eller andre tiltak og er nysgjerrig på vår virksomhet
+                  </p>
+                </div>
+                <div>
+                    <ContactBtn></ContactBtn>
+                </div>
+              </div>
+              <div className='h-2/6' >
+                <Image 
+                width={800}
+                alt="kodemiljø" 
+                src={danialigdery}
+                style={{objectFit: "contain"}}
+                ></Image>
+              </div>
             </div>
           </div>
-          <div className='h-2/6' >
-            <Image 
-            width={800}
-            alt="kodemiljø" 
-            src={danialigdery}
-            style={{objectFit: "contain"}}
-            ></Image>
+
+          <div className='flex flex-col gap-28' >
+
+              <div className='w-full flex justify-center'> 
+                <h1 className='font-extrabold text-5xl'>Hvordan kan vi hjelpe deg?</h1>
+              </div>
+
+              <div className='flex justify-center'>
+                <div className='flex flex-row gap-4'>
+                    <div className='flex flex-col gap-8 items-center'>
+                      <Image src={koffert2}></Image>
+                      <h5 className='text-center font-bold text-xl'>Bedrift</h5>
+                      <p className='w-3/6 text-center'>For bedrifter som er interessert i samarbeide</p>
+                      <SeMerBtn></SeMerBtn>
+                    </div>
+
+                    <div className='flex flex-col gap-8 items-center'>
+                      <Image src={hatt}></Image>
+                      <h5 className='text-center font-bold text-xl '>Kursdeltager</h5>
+                      <p className='w-3/6 text-center'>For deg som vil delta i Kodehode kurset</p>
+                      <SeMerBtn></SeMerBtn>
+                    </div>
+
+                    <div className='flex flex-col gap-8 mt-1  items-center'>
+                      <Image src={mappe}></Image>
+                      <h5 className='text-center  font-bold text-xl'>Virksomhet</h5>
+                      <p className='w-3/6 text-center'> For offentlig etat, skole, eller fylkeskommune</p>
+                      <SeMerBtn></SeMerBtn>
+                    </div>
+
+                </div>
+              </div>
+
+              <div className='flex gap-10 w-5/6 justify-center mb-20'>
+                <div className='w-3/6'>
+                  <Image src={visjonlanding2}></Image>
+                </div>
+
+                <div>
+                  <h2> Vår visjon</h2>
+                  <p> Her kommer noe tekst om visjon</p>
+                </div>
+
+              </div>
           </div>
-        </div>
-      </div>
-
-      <div className='flex justify-center'>
-         <div className='flex flex-row gap-40'>
-            <div className='flex flex-col'>
-             <Image src={briefcase}></Image>
-            </div>
-            <div className='flex flex-col'>
-            <Image src={cap}></Image>
-            </div>
-            <div className='flex flex-col'>
-            <Image src={folderkanban}></Image>
-            </div>
-
-         </div>
       </div>
     </div>
   )
