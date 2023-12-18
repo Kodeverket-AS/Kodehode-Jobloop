87d5f07705101ff363edee1ee2067c73846fcb49
RasmusKodehode <rasmus.kodehode@gmail.com>
2023-12-18 13:42:11 +0100

    styling summary mobile

--- components/KodehodeSummary.jsx
+++ components/KodehodeSummary.jsx
@@ -11,21 +11,25 @@ export default function KodehodeSummary({ content1, content2 }) {
   //console.log(content1);
   
   return (
-    <div className="flex flex-row py-20">
-      <div className="flex flex-col w-1/2">
-        <h1>{content1[0].title}</h1>
-        <PortableText value={content1[0].content} />
+    <div className="flex flex-col py-20 md:flex-row">
+      <div className="flex-col w-full md:w-1/2 md:flex">
+        <h1 className="hidden md:block">{content1[0].title}</h1>
+        <h1 className="block px-10 pb-5 md:hidden">Hvordan ser kurset ut:</h1>
+        <div className="hidden md:block">
+          <PortableText value={content1[0].content} />
+        </div>
       </div>
-      <div className="w-1/2 px-10">
+      <div className="w-full px-10 md:w-1/2">
         <div className="flex flex-row">
           {content2.map((item) => (
-              <button
-                key={item._id}
-                active={active === item}
-                onClick={() => setActive(item)}
-                className="px-3 py-2 mx-3 mb-3 border-2 border-black rounded-full active:bg-jobloop-orange"
-              >
-                {item.title}</button>
+            <button
+              key={item._id}
+              active={active === item}
+              onClick={() => setActive(item)}
+              className="px-3 py-2 mx-3 mb-3 border-2 border-black rounded-full active:bg-jobloop-orange"
+            >
+              {item.title}
+            </button>
           ))}
         </div>
         <div className="px-5 bg-white border-2 border-black shadow-md">
