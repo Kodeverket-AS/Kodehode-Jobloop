585e6e45c7c3e1572133b1ed6385d6e47f64622a
RasmusKodehode <rasmus.kodehode@gmail.com>
2023-12-21 10:38:37 +0100

    updated index in herosub

--- src/app/virksomhet/page.js
+++ src/app/virksomhet/page.js
@@ -17,7 +17,7 @@ export default async function Virksomhet() {
         <NavbarAlt subpage="Virksomhet" />
       </div>
       <main className="flex flex-col items-center min-h-screen p-24">
-        <HeroSub content={data.heroSub[2]} key={data.heroSub[2]._id} />
+        <HeroSub content={data.heroSub[3]} key={data.heroSub[3]._id} />
         <div className="flex flex-col items-center mt-80 md:mt-0">
           <KodehodeSummary content1={data.origin} content2={data.courseTypes} />
           <p>Denne siden er under oppbygging!</p>
