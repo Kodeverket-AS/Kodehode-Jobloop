66c4b7150fc201b555d7a8d80256e7965c3c0b35
RasmusKodehode <rasmus.kodehode@gmail.com>
2023-12-22 11:02:39 +0100

    styling course card

--- components/KodehodeSummary.jsx
+++ components/KodehodeSummary.jsx
@@ -14,7 +14,7 @@ export default function KodehodeSummary({ content1, content2 }) {
     <div className="flex flex-col py-20 md:flex-row">
       <div className="flex-col w-full md:w-1/2 md:flex">
         <h1 className="hidden md:block">{content1[0].title}</h1>
-        <h1 className="block px-10 pb-5 md:hidden">
+        <h1 className="block pb-5 md:hidden">
           {content1[0].mobileTitle}
         </h1>
         <div className="hidden md:block">
@@ -29,22 +29,20 @@ export default function KodehodeSummary({ content1, content2 }) {
           </div>
         </div>
       </div>
-      <div className="w-full px-10 md:w-1/2">
+      <div className="w-full px-0 md:px-10 md:w-1/2">
         <div className="flex flex-row justify-center">
           {content2.map((item) => (
             <button
               key={item._id}
               active={active === item}
               onClick={() => setActive(item)}
-              className="px-3 py-2 mx-3 mb-3 border-2 border-black rounded-full active:bg-jobloop-orange"
+              className="px-2 mx-3 mb-3 border-2 border-black rounded-full md:py-2 md:px-3 active:bg-jobloop-orange"
             >
               {item.duration}
             </button>
           ))}
         </div>
-        <div className="px-5 bg-white border-2 border-black shadow-md">
-          <CourseCard key={content2[0]._id} content={active} />
-        </div>
+        <CourseCard key={content2[0]._id} content={active} />
       </div>
     </div>
   );
