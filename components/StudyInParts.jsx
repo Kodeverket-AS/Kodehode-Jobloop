de9168311eb2ea5b9a0314bf37b8b1cd91d598e6
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-01-03 13:47:31 +0100

    styling courseParts section

--- components/StudyInParts.jsx
+++ components/StudyInParts.jsx
@@ -7,22 +7,22 @@ import { PortableText } from "@portabletext/react";
 export default function StudyInParts({ content }) {
   return (
     <>
-      <div className="flex flex-row bg-jobloop-green md:px-24">
-        <div className="flex flex-col w-1/2 py-4">
+      <div className="flex flex-col md:flex-row bg-jobloop-green md:px-24">
+        <div className="flex flex-col p-8 md:py-16 md:w-1/2">
           <h1>{content[0].title}</h1>
           <PortableText value={content[0].content} />
         </div>
-        <div className="w-1/2">
-          <Image src={content[0].image} width={500} height={500} className="mx-auto my-auto md:w-72" />
+        <div className="px-8 pb-8 md:py-20 md:w-1/2">
+          <Image src={content[0].image} width={500} height={500} className="mx-auto my-auto rounded md:w-72" />
         </div>
       </div>
-      <div className="flex flex-row-reverse md:px-24">
-        <div className="flex flex-col w-1/2 py-4">
+      <div className="flex flex-col md:flex-row-reverse md:px-24">
+        <div className="flex flex-col p-8 md:py-16 md:w-1/2">
           <h1>{content[1].title}</h1>
           <PortableText value={content[1].content} />
         </div>
-        <div className="w-1/2">
-          <Image src={content[1].image} width={500} height={500} className="mx-auto my-auto md:w-72" />
+        <div className="px-8 pb-8 md:py-20 md:w-1/2">
+          <Image src={content[1].image} width={500} height={500} className="mx-auto my-auto rounded md:w-72" />
         </div>
       </div>
     </>
