cf8dfd6130089e264a7a10246a61db48776129fe
Joakim Villo <villokodehode@gmail.com>
2024-01-05 12:54:03 +0100

    *

--- components/ProjectScroll.jsx
+++ components/ProjectScroll.jsx
@@ -9,23 +9,25 @@ export function ProjectScroll(content) {
 
   const data = content.content.map((item, index) => {
     return (
-      <div
-        className="w-[300px] h-[300px] p-10 overflow-hidden"
-        id={index}
-        key={item._id}
-      >
+      <div className="flex flex-col items-center justify-center w-full h-full p-10" id={index} key={item._id}>
         <div>
           <Image
             width={800}
             height={800}
-            className="w-full"
+            className="w-2/3 m-auto"
             src={item.ImageTwo}
             alt="image"
           />
         </div>
-        <div>
-          <p className="text-sm text-black">{item.description}</p>
-          {/* <PortableText className="text-black" item={item.content} /> */}
+        <div className="flex items-center justify-center">
+          {/* <p className="text-sm text-black">{item.description}</p> */}
+          {item.content.map((block, blockIndex) => (
+            <PortableText
+              className="scale-[0.7] text-black"
+              key={blockIndex}
+              content={block}
+            />
+          ))}
         </div>
       </div>
     );
@@ -33,20 +35,23 @@ export function ProjectScroll(content) {
 
   const responsiveSettings = {
     1: { items: 1 },
-    768: { items: 2 },
-    1024: { items: 3 },
-    1280: { items: 3 },
+    768: { items: 1 },
+    1024: { items: 1 },
+    1280: { items: 1 },
   };
 
   return (
-    <div className=" w-[320px] xl:lg:max-w-[1100px] lg:w-[1024px] md:w-[768px]">
-      <AliceCarousel
-        className="flex flex-col items-center justify-center"
-        items={data}
-        itemfit="cover"
-        responsive={responsiveSettings}
-      />
+    <div className="flex flex-col items-center justify-center py-16">
+      <div className="grid gap-7">
+        <div className="flex justify-center w-[320px] xl:lg:max-w-[1100px] lg:w-[1024px] md:w-[768px]">
+          <AliceCarousel
+            className="flex flex-col items-center justify-center h-fit"
+            items={data}
+            itemfit="contain"
+            responsive={responsiveSettings}
+          />
+        </div>
+      </div>
     </div>
   );
 }
-
