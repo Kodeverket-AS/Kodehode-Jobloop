a1266ca8a1187569ceeb57f0ccea86356e5e44a6
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-01-05 11:02:33 +0100

    trying to figure out respponsive issue that seems to be the same across the entire app

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ import kodeverketHvit from "../public/kodeverketHvit.png";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-row justify-between p-10 text-white bg-alt-purple">
+    <footer className=" flex flex-row justify-between p-10 text-white bg-alt-purple">
       <div className="px-20">
         <h2>Kontakt</h2>
         <div className="flex flex-col py-3">
@@ -26,10 +26,20 @@ export default function Footer() {
       <div className="flex flex-col px-20">
         <h2 className="mx-auto">Linker</h2>
         <div className="py-3 mx-auto">
-          <Image src={jobloopHvit} width={350} height={250} className="w-[150px]" />
+          <Image
+            src={jobloopHvit}
+            width={350}
+            height={250}
+            className="w-[150px]"
+          />
         </div>
         <div className="py-3 mx-auto">
-          <Image src={kodeverketHvit} width={350} height={250} className="w-[150px]" />
+          <Image
+            src={kodeverketHvit}
+            width={350}
+            height={250}
+            className="w-[150px]"
+          />
         </div>
       </div>
       <div className="flex flex-col px-20">
@@ -37,22 +47,19 @@ export default function Footer() {
         <Link
           href="/bedrift"
           aria-label="Gå til underside for bedrift"
-          className="py-3"
-        >
+          className="py-3">
           Bedrift
         </Link>
         <Link
           href="/deltaker"
           aria-label="Gå til underside for deltaker"
-          className="py-3"
-        >
+          className="py-3">
           Deltaker
         </Link>
         <Link
           href="/virksomhet"
           aria-label="Gå til underside for virksomhet"
-          className="py-3"
-        >
+          className="py-3">
           Virksomhet
         </Link>
       </div>
--- src/app/techstack/page.js
+++ src/app/techstack/page.js
@@ -1,22 +1,31 @@
 import Image from "next/image";
 import Footer from "../../../components/Footer";
 import Navbar from "../../../components/navigation/Navbar";
+import Wordcloud from "../../../public/wordcloud.png";
 
 export default function Techstack() {
   return (
     <main className="flex flex-col items-center min-h-screen p-24">
-      <section className="max-w-2xl flex flex-col gap-8">
-        <h1 className="font-head text-5xl font-bold text-jobloop-orange">
-          Våre kodespråk
-        </h1>
-        <p>
-          Våre studenter lærer de viktigste teknologiene og rammeverkene innen
-          frontend, og de har mulighet til å lære grunnleggende backend. Målet
-          er at de skal få en forståelse som gjør det enklere å lære seg nye
-          språk når det trengs, og før de begynner i praksis vil de få mulighet
-          til å få kjennskap til tech-stacken til deres bedrift slik at de kan
-          komme seg i gang så fort som mulig.
-        </p>
+      <section className="flex flex-col gap-12">
+        <div className="max-w-xl flex flex-col gap-8">
+          <h1 className="font-head text-5xl font-bold text-jobloop-orange">
+            Våre kodespråk
+          </h1>
+          <p>
+            Våre studenter lærer de viktigste teknologiene og rammeverkene innen
+            frontend, og de har mulighet til å lære grunnleggende backend. Målet
+            er at de skal få en forståelse som gjør det enklere å lære seg nye
+            språk når det trengs, og før de begynner i praksis vil de få
+            mulighet til å få kjennskap til tech-stacken til deres bedrift slik
+            at de kan komme seg i gang så fort som mulig.
+          </p>
+        </div>
+        <div className="max-w-5xl">
+          <Image
+            alt="Wordcloud of languages"
+            src={Wordcloud}
+          />
+        </div>
       </section>
     </main>
   );
