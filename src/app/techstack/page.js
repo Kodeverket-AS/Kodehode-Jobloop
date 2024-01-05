095a50af32d1dde64afc768251f90cbb08b2b203
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-01-05 14:29:14 +0100

    added comments in the global CSS file to reflect on possible minor change in the setup for easier global implementation.

--- src/app/techstack/page.js
+++ src/app/techstack/page.js
@@ -5,13 +5,23 @@ import Wordcloud from "../../../public/wordcloud.png";
 
 export default function Techstack() {
   return (
-    <main className="flex flex-col items-center min-h-screen py-8 px-4 md:p-24">
-      <section className="flex flex-col gap-14">
+    <main className="bg-white flex flex-col items-center min-h-screen py-8 px-4 md:p-24">
+      <section className="flex flex-col gap-32 pb-16">
         <div className="max-w-xl flex flex-col gap-6 md:gap-8">
-          <h1 className="font-head text-2xl font-bold text-jobloop-orange md:text-5xl">
+          <h1
+            className="font-head text-3xl font-bold md:text-5xl"
+            /*
+              MESSAGE FROM KRISTIAN:
+              Style below is added inline styling to change the color of the 
+              text because not defined/extended in tailwind or global css.
+              Color taken from figma design file.
+              Should update tailwind or global css with this color as main orange.
+              - Kristian
+            */
+            style={{ color: "#F4A366" }}>
             Våre kodespråk
           </h1>
-          <p className="text-base md:text-xl">
+          <p className="text-base text-gray-900 md:text-xl">
             Våre studenter lærer de viktigste teknologiene og rammeverkene innen
             frontend, og de har mulighet til å lære grunnleggende backend. Målet
             er at de skal få en forståelse som gjør det enklere å lære seg nye
