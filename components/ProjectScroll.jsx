0ab9bf3011829e321f8064bf87e0116ab05a59e2
Joakim Villo <villokodehode@gmail.com>
2024-01-05 12:12:51 +0100

    tried fixing

--- components/ProjectScroll.jsx
+++ components/ProjectScroll.jsx
@@ -1,220 +1,52 @@
-"use client"
-import AliceCarousel from "react-alice-carousel"
-import 'react-alice-carousel/lib/alice-carousel.css';
-import Image from "next/image"
+"use client";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import Image from "next/image";
 import PortableText from "react-portable-text";
 
+export function ProjectScroll(content) {
+  console.log(content.content);
 
-export function ScrollElement(content, Image, _id){
-  return(
-    
-    <div className="flex flex-col w-full p-10" > 
-
-        <div> 
-          <Image 
-          width={800}
-          height={800}
-          className="w-full"  
-          src={Image}/>
+  const data = content.content.map((item, index) => {
+    return (
+      <div
+        className="w-[300px] h-[300px] p-10 overflow-hidden"
+        id={index}
+        key={item._id}
+      >
+        <div>
+          <Image
+            width={800}
+            height={800}
+            className="w-full"
+            src={item.ImageTwo}
+            alt="image"
+          />
         </div>
-
-        <div> 
-          <PortableText content={content}></PortableText>
+        <div>
+          <p className="text-sm text-black">{item.description}</p>
+          {/* <PortableText className="text-black" item={item.content} /> */}
         </div>
-        
-     </div>
+      </div>
+    );
+  });
 
-  )
-}
+  const responsiveSettings = {
+    1: { items: 1 },
+    768: { items: 2 },
+    1024: { items: 3 },
+    1280: { items: 3 },
+  };
 
-
-
-
-export function ProjectScroll({ImageGlod, ImageFont, contentOne, contentTwo}){
- 
-let ImageAarray =[ 
-
-  <ScrollElement content={contentTwo} Image={ImageGlod} ></ScrollElement>
-  , 
-  
-  <ScrollElement content={contentOne} Image={ImageFont} ></ScrollElement>
-  
-
-
-
- 
- 
-      
-    ]  
-
- 
-
-
-// const data = content?.map((item) => {
-
-//   console.log(item.content[0])
-
-//   return(
-   
-//         <div key={item._id} className=" flex flex-col w-full"> 
-//                <div>
-//                   <p>{item.content}</p>
-//                 </div>
-//          </div> 
-    
-//   )
-
-  
-    
-// })
-
-const responsiveSettings = {
-  0: { items: 1 },
-  1024: { items: 3 },
-  1280: { items: 3 },
-};
-
-
-return(
-  <div className="w-full flex "> 
-    <AliceCarousel
-    className="flex flex-row items-center justify-center w-full overflow-hidden "
-    items={ImageAarray}
-    itemfit="cover"
-    responsive={responsiveSettings}
-    />
- </div>
-)
-
-
-
-
-
-}
-
-
-
-
-
-{/* <div className="flex flex-col items-center justify-center w-full ">
-    <div className="flex gap-7 w-full">
-      <div className="w-4/6 flex items-center">
-
-   
-
-
-
-       <Image
-                width={500}
-                height={500}
-                className="w-full" 
-                key={item._id}
-                scr={item.ImageTwo}></Image>
-        
+  return (
+    <div className=" w-[320px] xl:lg:max-w-[1100px] lg:w-[1024px] md:w-[768px]">
       <AliceCarousel
-        className="flex flex-col items-center justify-center overflow-hidden w-full "
+        className="flex flex-col items-center justify-center"
         items={data}
         itemfit="cover"
-        />
-        
-      </div>
+        responsive={responsiveSettings}
+      />
     </div>
-  </div> */}
+  );
+}
 
-
-  // bilde1
-    // bilde2
-    // tekst  
-    
-    // <div  className="bg-blue w-full"> 
-    // <>{data}</>
-    // </div>
-
-
-// export function ScrollElement({image, _id, text}){
-//     return(
-//         <div key={_id}>
-                
-//               /  <Image 
-//                 width={500}
-//                 height={500}
-//                 className="w-full" 
-//                 src={image}>
-
-//                 </Image>
-
-        
-
-//          </div>
-//     )
-// }
-
-// Array av data som et hentet inn
-
-
-// scroll funksjon
-
-
-
-   
-    
-//     const [scrollImage, setScrollImage]= useState(0)
-// //gjøre klikk til neste i array
-    
- 
-    
-    
-//     const nextProjectImage = () => {
-       
-//         if (scrollImage=== ImageAarray.length - 1) setScrollImage(0);
-//         else setScrollImage(scrollImage + 1);
-
-    
-//     }
-        
-//     return(
-//         <div key={_id} className="w-full flex flex-row"> 
-       
-//              {ImageAarray.map((index, image) => {
-            
-                
-//                    <Image
-//                     width={800}
-//                     height={800}
-//                     className="w-full" 
-//                     key={index}
-//                     src={image}
-//                     alt="samarbeidsprosjekter">
-                
-//                     </Image>
-
-//                console.log(<Image/>)
-                    
-//            })}
-             
-//             <button  className="w-10px bg-jobloop-orange">neste</button>
-//          </div>   
-         
-        
-//     )   
-        
-        
-    
-            
-    
-// }
-
-
-
-
-// i++
-// let newArr = textAray[i]
-// onClick={nextProjectImage}
-
-// let i = 0
-// const textAray =['hello ', 'My name is Anders', 'I am from Bergen', 'I am bad at coding']
-
-// setScrollImage(newArr)
-// if(textAray[i] > textAray.length ){ } else {setScrollImage(textAray[0])} else {setScrollImage(textAray[0])} if(textAray[i] > textAray.length-1 ){
-    
-// ImageFont={data.VisionSections[0].ImageTwo} ImageGlod={data.VisionSections[1].ImageTwo
--- src/app/visjon/page.js
+++ src/app/visjon/page.js
@@ -10,137 +10,138 @@ import { ProjectScroll } from "../../../components/ProjectScroll";
 
 
 export default async function Vision() {
-  const data= await getData()
-  
+  const data = await getData()
   return (
     <>
       <NavbarAlt />
-      <main className="flex flex-col items-center min-h-screen w-full">
-    
-          <div className="flex flex-col items-center sm:justify-center " >
-               <div className="flex flex-col items-center gap-40 lg:w-full w-4/6  mt-32 "> 
-                        <div className="flex flex-col  gap-12 w-4/6">
-                          <div>
-                            <h1 className="text-center text-jobloop-green font-extrabold">{data.heroSub[2].title}</h1>
-                          </div>
-                          <div key={data.heroSub[2]._id}>
-                            <Image  width={500}
-                                height={500}
-                                className="lg:w-full" 
-                                src={data.heroSub[2].image}>
-                          </Image>
-                          </div>
-                          <p> Her kommer tekst om vår visjon</p>
-                        </div>
+      <main className="flex flex-col items-center w-full min-h-screen">
 
-                        <div className="flex flex-col items-center justify-center hidden md:block ">
-                              <h1 className="p-20 font-extrabold">Samarbeidsprosjekter</h1>
-                              <div className="p-20 w-full bg-jobloop-orange flex justify-center  ">
-                                  <div className="w-5/6 flex flex-row gap-40 items-center ">
-                                      <div className="w-5/6 flex mt-20" key={data.VisionSections[1]._id}>
-                                          <Image 
-                                            width={500}
-                                            height={500}
-                                            className="w-full"  
-                                            src={data.VisionSections[1].ImageTwo}>
-                                          </Image>
-                                      </div>
-                                      <div className=" w-5/6 flex flex-col gap-10">
-                                        <Image 
-                                          width={100}
-                                          height={100}
-                                          className="w-3/6"  
-                                          src={data.VisionSections[1].ImageOne}>
-                                        </Image>
-                                          <PortableText content={data.VisionSections[1].content}></PortableText>
-                                          <SeMerBtn></SeMerBtn>
-                                      </div>
-                                  </div>  
-                               </div>
-                        
-                        </div>
-
-                        <div className="flex flex-col items-center justify-center">
-                              <div className="p-24 w-full flex justify-center hidden md:block ">
-                                  <div className="w-5/6 flex flex-row gap-40 items-center ">
-                                     <div className=" w-5/6 flex flex-col gap-10">
-                                        <Image 
-                                          width={100}
-                                          height={100}
-                                          className="w-4/6"  
-                                          src={data.VisionSections[0].ImageOne}>
-                                        </Image>
-                                          <PortableText content={data.VisionSections[0].content}></PortableText>
-                                          <SeMerBtn></SeMerBtn>
-                                      </div>
-
-                                      <div className="w-5/6 flex " key={data.VisionSections[0]._id}>
-                                          <Image 
-                                            width={500}
-                                            height={500}
-                                            className="w-full"  
-                                            src={data.VisionSections[0].ImageTwo}>
-                                          </Image>
-                                      </div>
-                                     
-                                  </div>  
-                               </div>
-                               <div>
-                                   <ProjectScroll key={data.VisionSections[1]._id} ImageFont={data.VisionSections[0].ImageTwo} ImageGlod={data.VisionSections[1].ImageTwo} contentOne={data.VisionSections[0].content} contentTwo={data.VisionSections[1].content} ></ProjectScroll>
-                               </div>
-                        </div>
-
-                        <div>
-                           <h1 className="font-extrabold text-center ">Hva partnere sier om oss</h1>
-                        </div>
-
-                        <EndorsementComp key={data.endorsement[0]._id} image={data.endorsement[0].image} navn={data.endorsement[0].navn} firma={data.endorsement[0].firma} content={data.endorsement[0].content}></EndorsementComp>
-                        <ContactComp 
-                        key={data.kontaktseksjon[0]._id} 
-                        imageOne={data.kontaktseksjon[0].ImageOne} 
-                        title={data.kontaktseksjon[0].title}
-                        content={data.kontaktseksjon[0].content}
-                        >
-                        </ContactComp>
-                        <div className="flex flex-col items-center w-full pb-32">
-                             <div>
-                               <h1 className="pb-20 font-extrabold text-center">Andre samarbeidspartnere</h1>
-                             </div>
-
-                            <div className="flex flex-row w-4/6 gap-32 justify-center pl-20">
-                                  <Partners
-                                  key={data.samarbeid[0]._id}
-                                  title={data.samarbeid[0].title}
-                                  image={data.samarbeid[0].image}
-                                  content={data.samarbeid[0].content}
-                                  >
-                                </Partners>
-
-                                <Partners
-                                  key={data.samarbeid[0]._id}
-                                  title={data.samarbeid[0].title}
-                                  image={data.samarbeid[0].image}
-                                  content={data.samarbeid[0].content}
-                                  >
-                                </Partners>
-
-                                <Partners
-                                  key={data.samarbeid[0]._id}
-                                  title={data.samarbeid[0].title}
-                                  image={data.samarbeid[0].image}
-                                  content={data.samarbeid[0].content}
-                                  >
-                                </Partners>
-
-                          </div>
-                       </div>
-                        
-                 </div> 
+        <div className="flex flex-col items-center sm:justify-center " >
+          <div className="flex flex-col items-center w-4/6 gap-40 mt-32 lg:w-full ">
+            <div className="flex flex-col w-4/6 gap-12">
+              <div>
+                <h1 className="font-extrabold text-center text-jobloop-green">{data.heroSub[2].title}</h1>
+              </div>
+              <div key={data.heroSub[2]._id}>
+                <Image width={500}
+                  height={500}
+                  className="lg:w-full"
+                  src={data.heroSub[2].image}>
+                </Image>
+              </div>
+              <p> Her kommer tekst om vår visjon</p>
             </div>
+
+            <div className="flex flex-col items-center justify-center hidden md:block ">
+              <h1 className="p-20 font-extrabold">Samarbeidsprosjekter</h1>
+              <div className="flex justify-center w-full p-20 bg-jobloop-orange ">
+                {/* <div className="flex flex-row items-center w-5/6 gap-40 ">
+                  <div className="flex w-5/6 mt-20" key={data.VisionSections[1]._id}>
+                    <Image
+                      width={500}
+                      height={500}
+                      className="w-full"
+                      src={data.VisionSections[1].ImageTwo}>
+                    </Image>
+                  </div>
+                  <div className="flex flex-col w-5/6 gap-10 ">
+                    <Image
+                      width={100}
+                      height={100}
+                      className="w-3/6"
+                      src={data.VisionSections[1].ImageOne}>
+                    </Image>
+                    <PortableText content={data.VisionSections[1].content}></PortableText>
+                    <SeMerBtn></SeMerBtn>
+                  </div>
+                </div> */}
+              </div>
+
+            </div>
+
+            <div className="flex flex-col items-center justify-center">
+              {/* <div className="flex justify-center hidden w-full p-24 md:block ">
+                <div className="flex flex-row items-center w-5/6 gap-40 ">
+                  <div className="flex flex-col w-5/6 gap-10 ">
+                    <Image
+                      width={100}
+                      height={100}
+                      className="w-4/6"
+                      src={data.VisionSections[0].ImageOne}>
+                    </Image>
+                    <PortableText content={data.VisionSections[0].content}></PortableText>
+                    <SeMerBtn></SeMerBtn>
+                  </div>
+
+                  <div className="flex w-5/6 " key={data.VisionSections[0]._id}>
+                    <Image
+                      width={500}
+                      height={500}
+                      className="w-full"
+                      src={data.VisionSections[0].ImageTwo}>
+                    </Image>
+                  </div>
+
+                </div>
+              </div> */}
+              <div>
+                <ProjectScroll
+                  content={data.VisionSections}>
+                  </ProjectScroll>
+              </div>
+            </div>
+
+            <div>
+              <h1 className="font-extrabold text-center ">Hva partnere sier om oss</h1>
+            </div>
+
+            <EndorsementComp key={data.endorsement[0]._id} image={data.endorsement[0].image} navn={data.endorsement[0].navn} firma={data.endorsement[0].firma} content={data.endorsement[0].content}></EndorsementComp>
+            <ContactComp
+              key={data.kontaktseksjon[0]._id}
+              imageOne={data.kontaktseksjon[0].ImageOne}
+              title={data.kontaktseksjon[0].title}
+              content={data.kontaktseksjon[0].content}
+            >
+            </ContactComp>
+            <div className="flex flex-col items-center w-full pb-32">
+              <div>
+                <h1 className="pb-20 font-extrabold text-center">Andre samarbeidspartnere</h1>
+              </div>
+
+              <div className="flex flex-row justify-center w-4/6 gap-32 pl-20">
+                <Partners
+                  key={data.samarbeid[0]._id}
+                  title={data.samarbeid[0].title}
+                  image={data.samarbeid[0].image}
+                  content={data.samarbeid[0].content}
+                >
+                </Partners>
+
+                <Partners
+                  key={data.samarbeid[0]._id}
+                  title={data.samarbeid[0].title}
+                  image={data.samarbeid[0].image}
+                  content={data.samarbeid[0].content}
+                >
+                </Partners>
+
+                <Partners
+                  key={data.samarbeid[0]._id}
+                  title={data.samarbeid[0].title}
+                  image={data.samarbeid[0].image}
+                  content={data.samarbeid[0].content}
+                >
+                </Partners>
+
+              </div>
+            </div>
+
+          </div>
+        </div>
 
       </main>
     </>
   );
 }
 
-{/* key={data.VisionSections[1]._id} ImageFont={data.VisionSections[0].ImageTwo} ImageGlod={data.VisionSections[1].ImageTwo */}
+{/* key={data.VisionSections[1]._id} ImageFont={data.VisionSections[0].ImageTwo} ImageGlod={data.VisionSections[1].ImageTwo */ }
