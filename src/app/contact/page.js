bd3bf51f5ef8883741c64f484677c53bc20a7b53
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-01-05 09:33:58 +0100

    test commit

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Contact() {
   return (
     <main className="flex flex-col items-center min-h-screen p-24">
-      <p>Denne siden er under oppbygging!</p>
+      <p>Denne siden er under oppbygging.</p>
       <p>Her kommer et kontaktskjema hos Kodehode</p>
     </main>
   );
