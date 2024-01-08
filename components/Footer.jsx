957559f596f4884d23b480dfae57ec5f50d6b09e
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-01-08 15:27:02 +0100

    deleted a lot of css globals to be replaced by default tailwind stuff. Also figuring out how to style the PortableText

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -29,10 +29,10 @@ const contactInformation = [
 
 export default function Footer() {
   return (
-    <footer className="text-white bg-alt-purple pb-8">
+    <footer className="text-white bg-jobloop-tertiary-purple pb-8">
       <div className=" flex flex-row justify-between items-start flex-wrap gap-y-20 gap-x-8 pt-8 pb-24 px-12  md:px-12">
         <div className="flex flex-col gap-8 ">
-          <h2 className="font-head">Kontakt</h2>
+          <h2 className="font-bold">Kontakt</h2>
           {contactInformation.map((person) => (
             <div
               className="flex flex-col "
@@ -55,7 +55,7 @@ export default function Footer() {
           ))}
         </div>
         <div className="flex flex-col items-center gap-8 ">
-          <h2 className="font-head text-center">Linker</h2>
+          <h2 className="font-bold text-center">Linker</h2>
           <div className="w-32">
             <Image
               alt="Jobloop logo"
@@ -74,7 +74,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="flex flex-col gap-8 ">
-          <h2 className="font-head">Sider</h2>
+          <h2 className="font-bold">Sider</h2>
           <Link
             className="hover:underline"
             href="/bedrift"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+
+      colors: {
+        jobloop: {
+          /* Hovedfarge fra Figma */
+          primay: {
+            green: "#77C4B4",
+            orange: "#F4A366",
+          },
+          /* Nedtonet fra Figma */
+          secondary: {
+            orange: "#f5b58a",
+            green: "#A5FBE9",
+            greentone: "#C8FFF3",
+          },
+          /* Sekundær fra Figma */
+          tertiary: {
+            purple: "#412F39",
+            green: "#1E555C",
+          },
+        },
+      },
+      /* Har ingen effekt foreløpig pga feil font import i layout main */
+      fontFamily: {
+        head: ["Futura", "Nunito Sans", "Roboto", "helvetica", "ui-sans"],
+        paragraph: ["Roboto, ui-sans"],
       },
     },
   },
+
   plugins: [],
-  theme: {
-    extend: {
-      colors: {
-        "alt-purple": "#412F39",
-        jobloop: {
-          green: "#77C4B4",
-          orange: "#F5B58A",
-        }
-      },
-      fontFamily: {
-        head: "Futura, ui-sans",
-        paragraph: "Roboto, ui-sans",
-      }
-    }
-  }
-}
+};
