86c08a730421e8686298cc2c90e770f170f46a10
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-01-09 11:11:29 +0100

    tried alot of different options, but the issue remains with trying to use a second Google font as headings. It works with local fonts.

--- src/app/techstack/page.js
+++ src/app/techstack/page.js
@@ -12,22 +12,8 @@ export default async function Techstack() {
     <main className=" flex flex-col items-center min-h-screen pt-12 pb-8 px-4 md:p-24">
       <section className="flex flex-col gap-32 pb-16">
         <div className="max-w-prose flex flex-col gap-6 md:gap-8 ">
-          <h1
-            className="text-3xl text-jobloop-primay-orange font-bold md:text-5xl"
-            /*
-            ok...doing this merge thing is a bit of a pain.
-            eehhh..did it work?
-              MESSAGE FROM KRISTIAN:
-              Style below is added inline styling to change the color of the 
-              text because not defined/extended in tailwind or global css.
-              Color taken from figma design file.
-              Should update tailwind or global css with this color as main orange.
-              - Kristian
-            */
-          >
-            {data.heroSub[4].title}
-          </h1>
-          <div className="text-base md:text-xl">
+          <h1 className="text-4xl  md:text-5xl">{data.heroSub[4].title}</h1>
+          <div className=" text-base md:text-xl">
             <PortableText value={data.heroSub[4].content} />
           </div>
         </div>
