434fbd539790fb95a8b346122b4f0860d693d1f6
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-01-10 10:05:52 +0100

    removed the navbar from the main Layout.js alltho i do think it is bad practice to do it this way and it should be reconsidered

--- src/app/techstack/page.js
+++ src/app/techstack/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { getData } from "../page";
 import { PortableText } from "@portabletext/react";
+import { NavbarAlt } from "../../../components/navigation/Navbar";
 
 export default async function Techstack() {
   const data = await getData();
@@ -9,23 +10,28 @@ export default async function Techstack() {
   }
 
   return (
-    <main className=" flex flex-col items-center min-h-screen pt-12 pb-8 px-4 md:p-24">
-      <section className="flex flex-col gap-32 pb-16">
-        <div className="max-w-prose flex flex-col gap-6 md:gap-8 ">
-          <h1 className="text-4xl  md:text-5xl">{data.heroSub[4].title}</h1>
-          <div className=" text-base md:text-xl">
-            <PortableText value={data.heroSub[4].content} />
+    <>
+      <NavbarAlt />
+      <main className=" flex flex-col items-center min-h-screen pt-12 pb-8 px-4 md:p-24">
+        <section className="flex flex-col gap-32 pb-16">
+          <div className="max-w-prose flex flex-col gap-6 md:gap-8 ">
+            <h1 className="text-4xl text-jobloop-primay-orange md:text-5xl">
+              {data.heroSub[4].title}
+            </h1>
+            <div className="text-base md:text-xl">
+              <PortableText value={data.heroSub[4].content} />
+            </div>
           </div>
-        </div>
-        <div className="max-w-5xl">
-          <Image
-            alt="Wordcloud of languages"
-            src={data.heroSub[4].image}
-            width={1000}
-            height={500}
-          />
-        </div>
-      </section>
-    </main>
+          <div className="max-w-5xl">
+            <Image
+              alt="Wordcloud of languages"
+              src={data.heroSub[4].image}
+              width={1000}
+              height={500}
+            />
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
