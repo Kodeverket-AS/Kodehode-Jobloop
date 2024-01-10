f86f891d7bcf967d33ceec558ae9da77c1da8122
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-01-10 12:47:13 +0100

    updated the Navigation to showcase how to display what page the user is currently visiting/viewing

--- components/navigation/Navigation.jsx
+++ components/navigation/Navigation.jsx
@@ -5,7 +5,17 @@ import Navbar, { NavbarAlt } from "./Navbar";
 
 const Navigation = () => {
   const pathname = usePathname();
-  return <div>{pathname === "/techstack" ? <NavbarAlt /> : <Navbar />}</div>;
+  return (
+    <div>
+      <div>{pathname === "/techstack" ? <NavbarAlt /> : <Navbar />}</div>
+      {/* Løsningen under kan brukes hvor somhelst for å vise hvilken side
+        brukeren befinner seg på. 
+        Importer usePathname fra next/navigation og bruk den i en div. */}
+      <div>
+        <p>du er på siden for: {pathname}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Navigation;
