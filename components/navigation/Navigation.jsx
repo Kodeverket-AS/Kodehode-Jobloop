b13142c7aa659e449dd4e014c3b87f709bd9d9a2
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-01-12 10:47:32 +0100

    Updated Navigation.jsx comment for the Team members to get a better understanding of what is happening here and why.

--- components/navigation/Navigation.jsx
+++ components/navigation/Navigation.jsx
@@ -10,6 +10,10 @@ const Navigation = () => {
     <div>
       {/* Spesifiser hvilken URL endpoint som skal definere hvilken Navbar
         som skal rendres utifra om pathname er true eller false. 
+        Vi kan også gjøre en if else statement eller switch case om vi trenger
+        flere alternativer for å definere hvilken Navbar som skal rendres hvor.
+        Å kjøre slug er jo også et alternativ, men foreløpig er dette en 
+        ok og lett-leselig måte å gjøre det på.
         */}
       <div>{pathname === "/techstack" ? <NavbarAlt /> : <Navbar />}</div>
       <MyPathname />
