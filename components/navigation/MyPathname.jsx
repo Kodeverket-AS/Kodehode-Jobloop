3f882574bbd03750d7c80e5e356c703fbc829936
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-01-15 13:12:48 +0100

    added more comments and descriptions

--- components/navigation/MyPathname.jsx
+++ components/navigation/MyPathname.jsx
@@ -1,11 +1,29 @@
 "use client";
-
 import React from "react";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+/**
+ * Denne komponenten viser hvilken URL vi er på
+ * Den setter basically en breadcrumb hvor du alltid kan gå tilbake til home.
+ * Kanksje den er overflødig, men den er fin å ha om den skulle trenges.
+ * Kan selvfølgelig styles som du vil og brukes hvor du vil.
+ * Men den gir mest mening i Navigeringen
+ */
 
 const MyPathname = () => {
   const pathname = usePathname();
-  return <div>MyPathname component is rendering this: {pathname}</div>;
+  return (
+    <div>
+      MyPathname component is rendering this:{" "}
+      <Link
+        href="./"
+        className="font-bold hover:underline">
+        Home
+      </Link>{" "}
+      {pathname}
+    </div>
+  );
 };
 
 export default MyPathname;
