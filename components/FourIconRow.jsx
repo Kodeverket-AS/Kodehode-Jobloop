cd318ca9eb0a7ffd008c41e0b958633d187f2b95
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-01-16 12:09:36 +0100

    styled techstack component

--- components/FourIconRow.jsx
+++ components/FourIconRow.jsx
@@ -5,28 +5,48 @@ import Image from "next/image";
 
 export default function FourIconRow({ content }) {
   return (
-    <div className="flex flex-col items-center w-full bg-jobloop-green">
-      <h1>{content[0].title}</h1>
-      <div className="flex justify-evenly">
-        <div className="flex flex-col items-center w-20 px-0">
-          <Image src={content[1].logo} width={300} height={300} />
+    <div className="flex flex-col items-center w-full py-10 bg-jobloop-green">
+      <h1 className="pb-10">{content[0].title}</h1>
+      <div className="flex">
+        <div className="flex flex-col items-center mr-4 w-14 md:mr-12 md:w-36">
+          <Image
+            src={content[1].logo}
+            width={300}
+            height={300}
+            className="w-20 mb-2"
+          />
           <h2>{content[1].title}</h2>
-          <p>{content[1].content}</p>
+          <p className="hidden text-center md:block">{content[1].content}</p>
         </div>
-        <div className="flex flex-col items-center w-20 px-0">
-          <Image src={content[2].logo} width={300} height={300} />
+        <div className="flex flex-col items-center mx-4 w-14 md:mx-12 md:w-36">
+          <Image
+            src={content[2].logo}
+            width={300}
+            height={300}
+            className="w-20 mb-2"
+          />
           <h2>{content[2].title}</h2>
-          <p>{content[2].content}</p>
+          <p className="hidden text-center md:block">{content[2].content}</p>
         </div>
-        <div className="flex flex-col items-center w-20 px-0">
-          <Image src={content[3].logo} width={300} height={300} />
+        <div className="flex flex-col items-center mx-4 w-14 md:mx-12 md:w-36">
+          <Image
+            src={content[3].logo}
+            width={300}
+            height={300}
+            className="w-20 mb-2"
+          />
           <h2>{content[3].title}</h2>
-          <p>{content[3].content}</p>
+          <p className="hidden text-center md:block">{content[3].content}</p>
         </div>
-        <div className="flex flex-col items-center w-20 px-0">
-          <Image src={content[4].logo} width={300} height={300} />
+        <div className="flex flex-col items-center ml-4 w-14 md:ml-12 md:w-36">
+          <Image
+            src={content[4].logo}
+            width={300}
+            height={300}
+            className="w-20 mb-2"
+          />
           <h2>{content[4].title}</h2>
-          <p>{content[4].content}</p>
+          <p className="hidden text-center md:block">{content[4].content}</p>
         </div>
       </div>
     </div>
