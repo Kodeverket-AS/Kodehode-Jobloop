ad1762825415aa5f7ec6e24f0b47f0a5d7e3aa3c
Øyvind A. Holm <oyvind.kodehode@gmail.com>
2024-01-23 12:56:31 +0100

    Start of landing page

---
+++ components/SeMerBtn.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export function SeMerBtn(){
+    return(
+
+            <button
+            className="rounded-3xl w-32 h-10 border-solid border-black border-2 bg-white transition-all duration-600  hover:bg-alt-purple hover:border-none hover:text-white"
+            >
+                Se Mer
+            </button>
+    )
+}
