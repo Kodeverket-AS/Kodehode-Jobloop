1ea8ae6ee22c2baf266224c125eda874825c9b94
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-01-25 11:30:52 +0100

    added existing components

--- src/app/bedrift/page.js
+++ src/app/bedrift/page.js
@@ -1,20 +1,32 @@
 import HeroSub from "../../../components/HeroSub";
 import { getData } from "../page";
 import EgenskapCoop from "../../../components/EgenskapCoop";
+import KodehodeSummary from "../../../components/KodehodeSummary";
+import Competence from "../../../components/Competence";
+import ContactComp from "../../../components/ContactComp";
+import LinkToFaq from "../../../components/LinkToFaq";
+import Link from "next/link";
 
 export default async function Bedrift() {
   const data = await getData();
   return (
     <>
       <main className="flex flex-col items-center min-h-screen p-24">
-        <HeroSub
-          content={data.heroSub[3]}
-          key={data.heroSub[3]._id}
-        />
+        <HeroSub content={data.heroSub[3]} key={data.heroSub[3]._id} />
         <div className="flex flex-col items-center mt-80 md:mt-0">
           <EgenskapCoop content={data.egsCoop} />
-          <p>Denne siden er under oppbygging!</p>
-          <p>Dette blir Viksomhetssiden til Kodehode</p>
+          <KodehodeSummary content1={data.origin} content2={data.courseTypes} />
+          <Competence
+            content1={data.relevantCompetence}
+            content2={data.competenceIcons}
+          />
+          <ContactComp 
+            key={data.kontaktseksjon[0]._id} 
+            imageOne={data.kontaktseksjon[0].ImageOne} 
+            title={data.kontaktseksjon[0].title}
+            content={data.kontaktseksjon[0].content}
+          />
+          <LinkToFaq />
         </div>
       </main>
     </>
