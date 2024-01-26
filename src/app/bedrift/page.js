ce283c0025bdd8473887cef520d1b84a57620e5c
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-01-26 12:53:02 +0100

    internship card added

--- src/app/bedrift/page.js
+++ src/app/bedrift/page.js
@@ -2,6 +2,7 @@ import HeroSub from "../../../components/HeroSub";
 import { getData } from "../page";
 import EgenskapCoop from "../../../components/EgenskapCoop";
 import KodehodeSummary from "../../../components/KodehodeSummary";
+import KodehodeInternship from "../../../components/KodehodeInternship";
 import Competence from "../../../components/Competence";
 import ContactComp from "../../../components/ContactComp";
 import LinkToFaq from "../../../components/LinkToFaq";
@@ -15,7 +16,7 @@ export default async function Bedrift() {
         <HeroSub content={data.heroSub[5]} key={data.heroSub[5]._id} />
         <div className="flex flex-col items-center mt-80 md:mt-20">
           <EgenskapCoop content={data.egsCoop} />
-          <KodehodeSummary content1={data.origin} content2={data.courseTypes} />
+          <KodehodeInternship content1={data.origin} content2={data.internship} />
           <Competence
             content1={data.relevantCompetence}
             content2={data.competenceIcons}
