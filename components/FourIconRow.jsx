95808adf6eab0049b79cff1513e810fa2ca5c7da
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-01-30 12:04:53 +0100

    finished ordering icons

--- components/FourIconRow.jsx
+++ components/FourIconRow.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function FourIconRow({ title, content }) {
   return (
-    <div className="flex flex-col items-center w-full py-10 bg-jobloop-primary-green">
+    <div className="flex flex-col items-center w-full py-10 text-white bg-jobloop-primary-green">
       <h1 className="pb-10">{title}</h1>
       <div className="flex">
         <div className="flex flex-col items-center mr-4 w-14 md:mr-12 md:w-36">
@@ -15,7 +15,7 @@ export default function FourIconRow({ title, content }) {
             height={300}
             className="w-20 mb-2"
           />
-          <h2>{content[0].title}</h2>
+          <h2 className="text-center">{content[0].title}</h2>
           <p className="hidden text-center md:block">{content[0].content}</p>
         </div>
         <div className="flex flex-col items-center mx-4 w-14 md:mx-12 md:w-36">
@@ -25,7 +25,7 @@ export default function FourIconRow({ title, content }) {
             height={300}
             className="w-20 mb-2"
           />
-          <h2>{content[1].title}</h2>
+          <h2 className="text-center">{content[1].title}</h2>
           <p className="hidden text-center md:block">{content[1].content}</p>
         </div>
         <div className="flex flex-col items-center mx-4 w-14 md:mx-12 md:w-36">
@@ -35,7 +35,7 @@ export default function FourIconRow({ title, content }) {
             height={300}
             className="w-20 mb-2"
           />
-          <h2>{content[2].title}</h2>
+          <h2 className="text-center">{content[2].title}</h2>
           <p className="hidden text-center md:block">{content[2].content}</p>
         </div>
         <div className="flex flex-col items-center ml-4 w-14 md:ml-12 md:w-36">
@@ -45,7 +45,7 @@ export default function FourIconRow({ title, content }) {
             height={300}
             className="w-20 mb-2"
           />
-          <h2>{content[3].title}</h2>
+          <h2 className="text-center">{content[3].title}</h2>
           <p className="hidden text-center md:block">{content[3].content}</p>
         </div>
       </div>
