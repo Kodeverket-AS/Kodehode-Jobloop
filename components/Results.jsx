bce65ef71c9be1475302d12444dff78c0381a757
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-02-02 14:06:37 +0100

    desktop news

--- components/Results.jsx
+++ components/Results.jsx
@@ -7,14 +7,14 @@ import { PortableText } from "@portabletext/react";
 export default function Results({ content }) {
     //console.log(content);
   return (
-    <div className="flex flex-row w-full py-20">
-      <div className="w-1/2 mr-0">
-        <Image src={content.image} width={500} height={500} className="mr-0 rounded" />
+    <div className="flex w-full px-20 py-20">
+      <div className="w-1/2">
+        <Image src={content.image} width={500} height={500} className="p-10 rounded" />
       </div>
-      <div className="flex flex-col w-1/2 pl-5">
-        <h1>Kodehode gir resultater</h1>
+      <div className="flex flex-col w-1/2 pr-40">
+        <h1 className="pt-10 pb-5">Kodehode gir resultater</h1>
         <PortableText value={content.content} />
-        <p>{content.person}</p>
+        <p className="py-5">{content.person}</p>
         <a href={content.link} target="blank" rel="noopener noreferrer" aria-label="lenke til artikkelen" className="underline text-jobloop-primary-green">Les mer på Kode24</a>
       </div>
     </div>
