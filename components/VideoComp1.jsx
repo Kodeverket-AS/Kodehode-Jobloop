5af57fe0219aaf3ccb34721a7a16907ad17b18f6
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-02-07 15:04:07 +0100

    removing image and hover effects

--- components/VideoComp1.jsx
+++ components/VideoComp1.jsx
@@ -7,12 +7,12 @@ import video1thumbnail from "../public/video1thumbnail.jpg";
 export default function InternshipCard() {
   return (
     <div className="w-full px-5 py-3 bg-jobloop-primary-green">
-      <Image
+      {/*<Image
         src={video1thumbnail}
         width={1000}
         height={500}
         className="block shadow-md hover:hidden"
-      />
+      />*/}
       <iframe
         width="1000"
         height="500"
@@ -21,7 +21,7 @@ export default function InternshipCard() {
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen
-        className="invisible shadow-md hover:visible"
+        className="shadow-md"
       ></iframe>
     </div>
   );
