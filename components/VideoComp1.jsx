5161a75599909d422a2186db6791309e5283ee1a
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-02-07 15:49:55 +0100

    styling video

--- components/VideoComp1.jsx
+++ components/VideoComp1.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import video1thumbnail from "../public/video1thumbnail.jpg";
 
-export default function InternshipCard() {
+export default function VideoComp1() {
   return (
     <div className="w-full px-5 py-3 bg-jobloop-primary-green">
       {/*<Image
@@ -15,13 +15,13 @@ export default function InternshipCard() {
       />*/}
       <iframe
         width="1000"
-        height="500"
-        src="https://www.youtube.com/embed/LocPs6zdkrQ?si=afaKRzcMA80WvwtW&amp;controls=0"
+        height="562"
+        src="https://www.youtube.com/embed/LocPs6zdkrQ?si=afaKRzcMA80WvwtW&amp;controls=0&autoplay=1&mute=1"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen
-        className="shadow-md"
+        className="mx-auto shadow-md"
       ></iframe>
     </div>
   );
