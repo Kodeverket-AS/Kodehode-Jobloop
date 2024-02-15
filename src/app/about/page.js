dcad0f90ff2f027f593856a09afb7b4622bcf1a0
steiningekodehode <steininge.kodehode@gmail.com>
2024-02-15 14:59:26 +0100

    fixed error

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,19 +5,22 @@ import Teachers from "../../../components/Teachers";
 
 
 export default async function About() {
-  //console.log('About function is running');
+ 
   const data = await getData();
-  //console.log(data.heroSub[7]);
+  const heroSubData = data.heroSub[7];
+  console.log(heroSubData);
+
   return (
     <>
       <main className="flex flex-col items-center min-h-screen p-24">
-      <HeroSub content={data.heroSub[7]} buttonText={"Kontakt oss"} key={data.heroSub[7]._id} />
+      <HeroSub 
+       content={heroSubData}
+       buttonText="Kontakt oss"
+       key={heroSubData._id} />
       
         <h1 className = "text-6xl font-bold text-jobloop-primary-green"></h1>
-        <p className="p-4 font-bold">
-          {data.content[7].content}
-        </p>
-        <p></p>
+        <p className="p-4 font-bold"></p>
+        
       </main>
     </>
   );
