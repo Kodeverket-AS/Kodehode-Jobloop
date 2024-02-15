1ac1d22d84ba0212453a7bf2ddf979999c7d19c6
steiningekodehode <steininge.kodehode@gmail.com>
2024-02-15 12:41:24 +0100

    test index

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,23 @@
-export default function About() {
+import HeroSub from "../../../components/HeroSub";
+import { getData } from "../page";
+import Offices from "../../../components/Offices";
+import Teachers from "../../../components/Teachers";
+
+
+export default async function About() {
+  //console.log('About function is running');
+  const data = await getData();
+  //console.log(data.heroSub[7]);
   return (
     <>
       <main className="flex flex-col items-center min-h-screen p-24">
-        <p className="p-4 font-bold bg-jobloop-green">
-          Denne siden er under oppbygging!
+      <HeroSub content={data.heroSub[7]} buttonText={"Kontakt oss"} key={data.heroSub[7]._id} />
+      
+        <h1 className = "text-6xl font-bold text-jobloop-primary-green"></h1>
+        <p className="p-4 font-bold">
+          {data.content[7].content}
         </p>
-        <p>Dette blir Om oss-siden til Kodehode</p>
+        <p></p>
       </main>
     </>
   );
