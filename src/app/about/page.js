080d283ec310ad1469be575ec1a198954ed06130
steiningekodehode <steininge.kodehode@gmail.com>
2024-02-16 15:33:56 +0100

    testing page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,25 +2,42 @@ import HeroSub from "../../../components/HeroSub";
 import { getData } from "../page";
 import Offices from "../../../components/Offices";
 import Teachers from "../../../components/Teachers";
+import JobloopPodium from "../../../components/JobloopPodium";
 
 
 export default async function About() {
  
   const data = await getData();
   const heroSubData = data.heroSub[7];
-  console.log(heroSubData);
-
-  return (
+  const selectedTeachersData = [data.personDatabase[0], data.personDatabase[1], data.personDatabase[6], data.personDatabase[7]];
+  console.log(data.about);
+return (
     <>
       <main className="flex flex-col items-center min-h-screen p-24">
       <HeroSub 
        content={heroSubData}
        buttonText="Kontakt oss"
        key={heroSubData._id} />
+
+      <div className="flex flex-col items-center ">
+        <h2>Vårt team</h2>
+        <Teachers
+        content={selectedTeachersData} 
+        />
+         </div>
+      <div className="flex flex-col items-center ">
+        <JobloopPodium content={data.about}/>
+        </div>
+      <div className="flex flex-col items-center ">
+        <Offices
+        content={data.OurLocals} 
+        />
+      </div>
+
+         
+         
+
       
-        <h1 className = "text-6xl font-bold text-jobloop-primary-green"></h1>
-        <p className="p-4 font-bold"></p>
-        
       </main>
     </>
   );
