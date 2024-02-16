1ce46ba5d32a72aff408a0ead85171af102a326a
AndyKodehode <andykodehode@gmail.com>
2024-02-16 09:50:49 +0100

    mobile version

--- src/app/FAQ/page.js
+++ src/app/FAQ/page.js
@@ -7,6 +7,7 @@ import { FaqBedrift } from "../../../components/FAQcomp";
 import { FaqVirk } from "../../../components/FAQcomp";
 import { getData } from "../page";
 import { ToggleBtn } from "../../../components/Buttons";
+import HeroSub from "../../../components/HeroSub";
 
 
 
@@ -19,7 +20,7 @@ export default async function FAQ() {
 
   const faqData = await getData('FAQ');
   
-  
+  const HeroData = await getData('heroSub');
 
 
 
@@ -28,6 +29,7 @@ export default async function FAQ() {
    return (
     <>
       <main className="flex flex-col items-center min-h-screen p-24">
+        {/* <HeroSub content={HeroData.heroSub[3]}></HeroSub> */}
         <h1 className="font-bold text-3xl text-center">Spørsmål og svar</h1>
         <div className="flex flex-row gap-10 ">
           <ToggleBtn titleDel={faqData.FAQ[0].title} titleBed={faqData.FAQ[1].title} titleVirk={faqData.FAQ[2].title} contentQuestOne={faqData.FAQ[0].question} contentSvarOne={faqData.FAQ[0].svar} idOne={faqData.FAQ[0]._id}  contentQuestTwo={faqData.FAQ[1].question} contentSvarTwo={faqData.FAQ[1].svar} idTwo={faqData.FAQ[1]._id} contentQuestThree={faqData.FAQ[2].question} contentSvarThree={faqData.FAQ[2].svar} idThree={faqData.FAQ[2]._id}></ToggleBtn>
