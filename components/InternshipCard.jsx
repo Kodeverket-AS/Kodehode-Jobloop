ac7864110e156b25fb083442d13cd2a34a4e3d18
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-02-16 11:20:16 +0100

    adjusted padding

--- components/InternshipCard.jsx
+++ components/InternshipCard.jsx
@@ -8,15 +8,15 @@ export default function InternshipCard({ content }) {
     <div className="flex flex-col px-5 py-3 bg-white shadow-md">
       <h3>{content.title}</h3>
       <p className="pb-3 border-b-4 border-black">{content.subheader}</p>
-      <h4>Forutsetninger</h4>
+      <h4 className="pt-2">Forutsetninger</h4>
       <p>{content.expectations}</p>
-      <h4>Tidsplan</h4>
+      <h4 className="pt-2">Tidsplan</h4>
       <p>{content.schedule}</p>
-      <h4>Veiledning</h4>
+      <h4 className="pt-2">Veiledning</h4>
       <p>{content.guidance}</p>
-      <h4>Tilbud om fast stilling?</h4>
+      <h4 className="pt-2">Tilbud om fast stilling?</h4>
       <p>{content.jobOffer}</p>
-      <h4>Hjelp til å bli godkjent lærlingbedrift</h4>
+      <h4 className="pt-2">Hjelp til å bli godkjent lærlingbedrift</h4>
       <p>{content.help}</p>
     </div>
   );
