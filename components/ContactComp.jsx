deb511e47606c20c2e6e4e3c5a33a8bdebc56846
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-02-16 14:26:54 +0100

    fixed contact button

--- components/ContactComp.jsx
+++ components/ContactComp.jsx
@@ -4,33 +4,25 @@ import Image from "next/image"
 import { ContactBtn, LinkButtonColour } from "./Buttons"
 
 export default function ContactComp({imageOne, title, content, _id}){
-    return(
-        
-        <div className='flex justify-center p-20 bg-jobloop-primary-green'>
-         <div className="w-5/6">
-                <div className="flex flex-col w-full gap-10 md:flex-row md:gap-7 ">
-                    <div className="flex flex-row w-full ">
-                        <div className="w-full">
-                            <Image 
-                            width={400}
-                            height={400}
-                            src={imageOne}
-                                
-                            ></Image>
-                        </div>
-                     
-                      
-                    </div>
-
-                    <div className="flex flex-col justify-center w-full gap-10 md:w-4/6">
-                        <h2 className="font-extrabold">{title}</h2>
-                        <PortableText value={content} />
-                        <LinkButtonColour Path={"/contact"} Text={"Kontakt oss"} /> 
-                    </div>
-               </div>
+    return (
+      <div className="flex justify-center p-20 bg-jobloop-primary-green">
+        <div className="w-5/6">
+          <div className="flex flex-col w-full gap-10 md:flex-row md:gap-7 ">
+            <div className="flex flex-row w-full ">
+              <div className="w-full">
+                <Image width={400} height={400} src={imageOne}></Image>
+              </div>
             </div>
+
+            <div className="flex flex-col justify-center w-full gap-10 md:w-4/6">
+              <h2 className="font-extrabold">{title}</h2>
+              <PortableText value={content} />
+              <div className="">
+                <LinkButtonColour Path={"/contact"} Text={"Kontakt oss"} />
+              </div>
+            </div>
+          </div>
         </div>
-         
-    
-    )
+      </div>
+    );
 }
