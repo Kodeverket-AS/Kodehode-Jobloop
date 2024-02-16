7dedfc745737aa699822619e775e55ded750e8a7
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-02-16 14:54:07 +0100

    button links fixed and text added

--- src/app/page.js
+++ src/app/page.js
@@ -9,88 +9,119 @@ import koffert2 from '../../public/koffert2.png'
 import hatt from '../../public/hatt.png'
 import { SeMerBtn } from '../../components/SeMerBtn';
 import visjonlanding2 from '../../public/visjonlanding2.jpeg'
+import { LinkButton } from '../../components/Buttons';
 
 export default function Home() {
   return (
     <div>
       <div>
-      <LandingHero></LandingHero>
+        <LandingHero></LandingHero>
       </div>
-      <div className='flex flex-col gap-32'>
-          <div className='flex justify-center w-screen '>
-            <div className='flex flex-row justify-center w-5/6 gap-x-10' >
-              <div className='flex flex-col w-3/6 gap-20'>
-                <div>
-                  <p>Vi rekrutterer kandidater som av ulike grunner har havnet utenfor arbeidslivet og lærer dem opp til å bli frontend utviklere.
-
-                  Gjennom en kombinasjon av teoretisk opplæring og praksis vil kandidatene lære seg de viktigste teknologiene og bli klar for en karriere innen utvikling.
-
-                  Vi vil gjerne høre fra deg, enten du vil bli et kodehode, eller om du er en bedrift som vil samarbeide med oss og hjelpe våre kandidater, og om du er fra NAV eller andre tiltak og er nysgjerrig på vår virksomhet
-                  </p>
-                </div>
-                <div>
-                    <ContactBtn></ContactBtn>
-                </div>
+      <div className="flex flex-col gap-32">
+        <div className="flex justify-center w-screen ">
+          <div className="flex flex-row justify-center w-5/6 gap-x-10">
+            <div className="flex flex-col w-3/6 gap-20">
+              <div>
+                <p>
+                  Vi rekrutterer kandidater som av ulike grunner har havnet
+                  utenfor arbeidslivet og lærer dem opp til å bli frontend
+                  utviklere. Gjennom en kombinasjon av teoretisk opplæring og
+                  praksis vil kandidatene lære seg de viktigste teknologiene og
+                  bli klar for en karriere innen utvikling. Vi vil gjerne høre
+                  fra deg, enten du vil bli et kodehode, eller om du er en
+                  bedrift som vil samarbeide med oss og hjelpe våre kandidater,
+                  og om du er fra NAV eller andre tiltak og er nysgjerrig på vår
+                  virksomhet
+                </p>
               </div>
-              <div className='h-2/6' >
-                <Image
+              <div>
+                <ContactBtn></ContactBtn>
+              </div>
+            </div>
+            <div className="h-2/6">
+              <Image
                 width={800}
                 alt="kodemiljø"
                 src={danialigdery}
-                style={{objectFit: "contain"}}
-                ></Image>
+                style={{ objectFit: "contain" }}
+              ></Image>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-28">
+          <div className="flex justify-center w-full">
+            <h1 className="text-5xl font-extrabold">
+              Hvordan kan vi hjelpe deg?
+            </h1>
+          </div>
+
+          <div className="flex justify-center">
+            <div className="flex flex-row gap-4">
+              <div className="flex flex-col items-center gap-8">
+                <Image src={koffert2}></Image>
+                <h5 className="text-xl font-bold text-center">Arbeidsgiver</h5>
+                <p className="w-3/6 text-center">
+                  For bedrifter som er interessert i samarbeide
+                </p>
+                <div>
+                  <LinkButton Path={"/arbeidsgiver"} Text={"Se mer"} />
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center gap-8">
+                <Image src={hatt}></Image>
+                <h5 className="text-xl font-bold text-center ">Deltager</h5>
+                <p className="w-3/6 text-center">
+                  For deg som vil delta i Kodehode kurset
+                </p>
+                <div>
+                  <LinkButton Path={"/deltaker"} Text={"Se mer"} />
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center gap-8 mt-1">
+                <Image src={mappe}></Image>
+                <h5 className="text-xl font-bold text-center">
+                  Samarbeidspartner
+                </h5>
+                <p className="w-3/6 text-center">
+                  {" "}
+                  For offentlig etat, skole, eller fylkeskommune
+                </p>
+                <div>
+                  <LinkButton Path={"/samarbeidspartner"} Text={"Se mer"} />
+                </div>
               </div>
             </div>
           </div>
 
-          <div className='flex flex-col gap-28' >
+          <div className="flex justify-center w-5/6 gap-10 mb-20">
+            <div className="w-3/6">
+              <Image src={visjonlanding2}></Image>
+            </div>
 
-              <div className='flex justify-center w-full'>
-                <h1 className='text-5xl font-extrabold'>Hvordan kan vi hjelpe deg?</h1>
+            <div className="flex flex-col gap-4">
+              <h2>Inkludering lønner seg.</h2>
+              <p>
+                Andelen unge i utenforskap med interesse for gaming og IT i
+                Norge øker, det samme gjør behovet for IT-talent og utvikler i
+                fremtiden. Vår visjon er å skape verdi for samfunn og næringsliv
+                ved å redusere utenforskap gjennom lønnsom opplæring,
+                rekruttering, matchmaking og onboarding innen IT-faget. Vi
+                kjemper for et mer inkluderende samfunn, og å skinne lys på at
+                inkludering ikke behøver å være en utgift, men en verdi for
+                bedrifter såvel som samfunnet som helhet.
+              </p>
+              <div>
+                <LinkButton Path={"/visjon"} Text={"Se mer"} />
               </div>
-
-              <div className='flex justify-center'>
-                <div className='flex flex-row gap-4'>
-                    <div className='flex flex-col items-center gap-8'>
-                      <Image src={koffert2}></Image>
-                      <h5 className='text-xl font-bold text-center'>Bedrift</h5>
-                      <p className='w-3/6 text-center'>For bedrifter som er interessert i samarbeide</p>
-                      <SeMerBtn></SeMerBtn>
-                    </div>
-
-                    <div className='flex flex-col items-center gap-8'>
-                      <Image src={hatt}></Image>
-                      <h5 className='text-xl font-bold text-center '>Kursdeltager</h5>
-                      <p className='w-3/6 text-center'>For deg som vil delta i Kodehode kurset</p>
-                      <SeMerBtn></SeMerBtn>
-                    </div>
-
-                    <div className='flex flex-col items-center gap-8 mt-1'>
-                      <Image src={mappe}></Image>
-                      <h5 className='text-xl font-bold text-center'>Virksomhet</h5>
-                      <p className='w-3/6 text-center'> For offentlig etat, skole, eller fylkeskommune</p>
-                      <SeMerBtn></SeMerBtn>
-                    </div>
-
-                </div>
-              </div>
-
-              <div className='flex justify-center w-5/6 gap-10 mb-20'>
-                <div className='w-3/6'>
-                  <Image src={visjonlanding2}></Image>
-                </div>
-
-                <div className='flex flex-col gap-4'>
-                  <h2> Vår visjon</h2>
-                  <p> Her kommer noe tekst om visjon</p>
-                  <SeMerBtn></SeMerBtn>
-                </div>
-
-              </div>
+            </div>
           </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export async function getData() {
