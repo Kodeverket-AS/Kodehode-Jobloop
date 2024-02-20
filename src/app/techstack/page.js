0d2ff6f7d929e42507a8a88d6a3f8a55128f95f9
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2024-02-20 12:15:51 +0100

    add coloursplash in tr corner

--- src/app/techstack/page.js
+++ src/app/techstack/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { getData } from "../page";
 import { PortableText } from "@portabletext/react";
+import Fargefigur180 from "../../../public/Fargefigur180.svg";
 
 export default async function Techstack() {
   const data = await getData();
@@ -10,7 +11,15 @@ export default async function Techstack() {
 
   return (
     <>
-      <main className="flex flex-col items-center min-h-screen px-4 pt-12 pb-8 md:p-24">
+      <main className="relative flex flex-col items-center min-h-screen px-4 pt-12 pb-8 md:p-24">
+        <div>
+          <Image
+            src={Fargefigur180}
+            width={1000}
+            height={1000}
+            className="hidden md:block absolute top-[-200px] right-0 w-2/5 mr-0 z-[0]"
+          />
+        </div>
         <section className="flex flex-col gap-32 pb-16">
           <div className="flex flex-col gap-6 max-w-prose md:gap-8 ">
             <h1 className="text-4xl text-jobloop-primay-orange md:text-5xl">
