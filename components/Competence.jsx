28e2436fdf3ed95ea243476fc4d1b650dca420d2
KristofferG <kristofferb04.kodehode@gmail.com>
2024-02-20 09:30:28 +0100

    adjust spacing for mobile

--- components/Competence.jsx
+++ components/Competence.jsx
@@ -16,24 +16,24 @@ export default function Competence({ content1, content2 }) {
       </div>
       <div className="flex flex-col pt-0 pb-10 md:flex-row md:py-10">
         <div className="flex flex-row">
-          <div className="flex flex-col items-center mx-5 w-14 md:mr-12 md:w-48">
+          <div className="flex flex-col items-center mx-9 w-14 md:mx-12 md:w-48">
             <Image src={content2[0].logo} width={300} height={300} className="w-auto h-24 mb-2" />
             <h3 className="text-center">{content2[0].title}</h3>
             <p className="hidden text-center md:block">{content2[0].content}</p>
           </div>
-          <div className="flex flex-col items-center mx-5 w-14 md:mx-12 md:w-48">
+          <div className="flex flex-col items-center mx-9 w-14 md:mx-12 md:w-48">
             <Image src={content2[1].logo} width={300} height={300} className="w-auto h-24 mb-2" />
             <h3 className="text-center">{content2[1].title}</h3>
             <p className="hidden text-center md:block">{content2[1].content}</p>
           </div>
         </div>
         <div className="flex flex-row">
-          <div className="flex flex-col items-center mx-5 w-14 md:mx-12 md:w-48">
+          <div className="flex flex-col items-center mx-9 w-14 md:mx-12 md:w-48">
             <Image src={content2[2].logo} width={300} height={300} className="w-auto h-24 mb-2" />
             <h3 className="text-center">{content2[2].title}</h3>
             <p className="hidden text-center md:block">{content2[2].content}</p>
           </div>
-          <div className="flex flex-col items-center mx-5 w-14 md:ml-12 md:w-48">
+          <div className="flex flex-col items-center mx-9 w-14 md:ml-12 md:w-48">
             <Image src={content2[3].logo} width={300} height={300} className="w-auto h-24 mb-2" />
             <h3 className="text-center">{content2[3].title}</h3>
             <p className="hidden text-center md:block">{content2[3].content}</p>
