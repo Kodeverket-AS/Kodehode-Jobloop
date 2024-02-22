d860a91a9d357fb31bd5ac72daf2471d9e912371
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-02-22 12:58:44 +0100

    styling result component and adjusted contactcomp

--- components/Results.jsx
+++ components/Results.jsx
@@ -7,12 +7,12 @@ import { PortableText } from "@portabletext/react";
 export default function Results({ content }) {
     //console.log(content);
   return (
-    <div className="flex w-full px-5 py-20 md:px-20">
-      <div className="w-2/5 md:w-1/2">
-        <Image src={content.image} width={500} height={500} className="p-3 rounded md:p-10" />
+    <div className="flex flex-col w-full px-8 py-20 sm:px-10 md:px-12 lg:px-24 sm:flex-row">
+      <div className="w-full sm:w-1/2">
+        <Image src={content.image} width={500} height={500} className="pt-0 pr-0 rounded sm:pr-3 md:pr-10 sm:pt-3" />
       </div>
-      <div className="flex flex-col w-3/5 pr-0 md:w-1/2 md:pr-40">
-        <h2 className="pt-3 pb-5 md:pt-10">Kodehode gir resultater</h2>
+      <div className="flex flex-col w-full pl-0 pr-0 sm:w-1/2 sm:pl-5">
+        <h2 className="pt-3 pb-5 sm:pt-0">Kodehode gir resultater</h2>
         <PortableText value={content.content} />
         <p className="py-5">{content.person}</p>
         <a href={content.link} target="blank" rel="noopener noreferrer" aria-label="lenke til artikkelen" className="underline text-jobloop-primary-green">Les mer på Kode24</a>
