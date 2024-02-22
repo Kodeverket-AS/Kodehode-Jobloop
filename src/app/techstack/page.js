10ccfa3806b6c55640f3eb6282e65ff99b11f991
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2024-02-22 10:10:33 +0100

    adjust coloursplash size on 3xl

--- src/app/techstack/page.js
+++ src/app/techstack/page.js
@@ -15,7 +15,7 @@ export default async function Techstack() {
         src={Fargefigur180}
         width={1000}
         height={1000}
-        className="hidden md:block absolute top-[0] lg:top-[-100px] right-0 md:w-1/2 xl:w-2/5 mr-0 z-[-1]"
+        className="hidden md:block absolute top-[0] lg:top-[-100px] right-0 md:w-1/2 xl:w-2/5 3xl:w-2/6 mr-0 z-[-1]"
         alt=""
       />
       <main className="relative flex flex-col items-center min-h-screen px-4 pt-12 pb-8 md:p-24">
