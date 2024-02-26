212d317de92f93bcde2b830b00cf4efd3394a605
KristofferG <kristofferb04.kodehode@gmail.com>
2024-02-26 12:39:52 +0100

    add external link button

--- components/Buttons.jsx
+++ components/Buttons.jsx
@@ -55,7 +55,7 @@ export function SeMerBtn() {
       Se Mer
     </button>
   );
-} 
+}
 
 export function LinkButton({ Path, Text }) {
   return (
@@ -68,18 +68,36 @@ export function LinkButton({ Path, Text }) {
   );
 }
 
+export function ExternalLinkButton({ Path, Text, ariaLabel, background }) {
+  return (
+    <a
+      href={Path}
+      rel="noopener noreferrer"
+      aria-label={ariaLabel}
+      className={`px-8 py-2 transition-all ${
+        background == "colour" ? "bg-white" : "bg-jobloop-primary-orange"
+      } text-black
+      } border-2 border-black border-solid max-w-fit rounded-3xl hover:${
+        background == "colour" ? "bg-black" : "bg-white"
+      } `}
+    >
+      {Text}
+    </a>
+  );
+}
+
 export function JobloopPodiumButton({ Path, ariaLabel, ButtonText }) {
   return (
-    
     <a
-              href={Path}
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label={ariaLabel}
-              className="px-8 py-2 transition-all bg-white border-2 border-black border-solid rounded-3xl hover:bg-jobloop-primary-orange hover:border-none hover:text-white">
-              {ButtonText}
+      href={Path}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label={ariaLabel}
+      className="px-8 py-2 transition-all bg-white border-2 border-black border-solid rounded-3xl hover:bg-jobloop-primary-orange hover:border-none hover:text-white"
+    >
+      {ButtonText}
     </a>
-    );
+  );
 }
 
 export function LinkButtonColour({ Path, Text }) {
@@ -93,93 +111,148 @@ export function LinkButtonColour({ Path, Text }) {
   );
 }
 
-
-export function ToggleBtn({titleDel, titleBed, titleVirk, idOne, idTwo, idThree, contentQuestOne, contentSvarOne, contentQuestTwo, contentSvarTwo, contentQuestThree, contentSvarThree}) {
+export function ToggleBtn({
+  titleDel,
+  titleBed,
+  titleVirk,
+  idOne,
+  idTwo,
+  idThree,
+  contentQuestOne,
+  contentSvarOne,
+  contentQuestTwo,
+  contentSvarTwo,
+  contentQuestThree,
+  contentSvarThree,
+}) {
   const [isDelVisible, setDelIsVisible] = useState(true);
   const [isBedVisible, setBedIsVisible] = useState(false);
-  const [isVirkVisible, setVirkIsVisible] = useState(false)
-
+  const [isVirkVisible, setVirkIsVisible] = useState(false);
 
   function handleDelToggle() {
     setDelIsVisible(!isDelVisible);
-    setBedIsVisible(false)
-    setVirkIsVisible(false)
-    
-  };
+    setBedIsVisible(false);
+    setVirkIsVisible(false);
+  }
 
   function handleBedToggle() {
     setBedIsVisible(!isBedVisible);
-    setDelIsVisible(false)
-    setVirkIsVisible(false)
-    
-  };
+    setDelIsVisible(false);
+    setVirkIsVisible(false);
+  }
 
   function handleVirkToggle() {
     setVirkIsVisible(!isVirkVisible);
-    setDelIsVisible(false)
-    setBedIsVisible(false)
-    
-  };
+    setDelIsVisible(false);
+    setBedIsVisible(false);
+  }
 
   return (
     <div className="flex flex-col w-full gap-7">
-
       <div className="flex justify-center w-full">
-
         <div className="flex flex-col items-start justify-center w-3/6 gap-4 ml-24 md:w-full md:flex-row md:p-20 md:gap-7">
-                    <div>
-                        <button onClick={handleDelToggle} className={`md:p-7 flex flex-row gap-3 items-start md:items-center rounded-3xl lg:w-44 w-20 h-7 lg:h-10 border-solid border-black border-2  transition-all duration-600 text-sm
-                        ${isDelVisible ?'border-jobloop-primary-orange border-none md:border-solid  md:border-2 '  : ' border-none'}`}>  
-                          <div className={`flex items-center justify-center w-4 h-4 md:w-7 md:h-7  rounded-full ${isDelVisible ?' bg-jobloop-primary-orange ': ' bg-gray-200'}`} >
-                            <div className={`w-2 h-2 rounded-full ${isDelVisible ?' bg-white ': ' bg-transparent'}`} > </div>
-                          </div>
-                        <p>{titleDel}</p>
-                        </button>
-                    </div>
+          <div>
+            <button
+              onClick={handleDelToggle}
+              className={`md:p-7 flex flex-row gap-3 items-start md:items-center rounded-3xl lg:w-44 w-20 h-7 lg:h-10 border-solid border-black border-2  transition-all duration-600 text-sm
+                        ${
+                          isDelVisible
+                            ? "border-jobloop-primary-orange border-none md:border-solid  md:border-2 "
+                            : " border-none"
+                        }`}
+            >
+              <div
+                className={`flex items-center justify-center w-4 h-4 md:w-7 md:h-7  rounded-full ${
+                  isDelVisible ? " bg-jobloop-primary-orange " : " bg-gray-200"
+                }`}
+              >
+                <div
+                  className={`w-2 h-2 rounded-full ${
+                    isDelVisible ? " bg-white " : " bg-transparent"
+                  }`}
+                >
+                  {" "}
+                </div>
+              </div>
+              <p>{titleDel}</p>
+            </button>
+          </div>
 
-                    <div>
-                        <button onClick={handleBedToggle} className={`md:p-7 flex flex-row gap-3 items-start md:items-center rounded-3xl w-36 md:w-56  h-7 lg:h-10 border-solid border-black border-2  transition-all duration-600 text-sm
-                        ${isBedVisible ?'border-jobloop-primary-orange border-none md:border-solid  md:border-2 '  : ' border-none'}`}>
-                        <div className={` flex items-center justify-center w-4 h-4 md:w-7 md:h-7   rounded-full ${isBedVisible ?' bg-jobloop-primary-orange ': ' bg-gray-200'}`} >
-                            <div className={`w-2 h-2 rounded-full ${isBedVisible ?' bg-white ': ' bg-transparent'}`} > </div>
-                        </div>
-                        <p>{titleBed}</p>
-                        </button>
-                    </div>
+          <div>
+            <button
+              onClick={handleBedToggle}
+              className={`md:p-7 flex flex-row gap-3 items-start md:items-center rounded-3xl w-36 md:w-56  h-7 lg:h-10 border-solid border-black border-2  transition-all duration-600 text-sm
+                        ${
+                          isBedVisible
+                            ? "border-jobloop-primary-orange border-none md:border-solid  md:border-2 "
+                            : " border-none"
+                        }`}
+            >
+              <div
+                className={` flex items-center justify-center w-4 h-4 md:w-7 md:h-7   rounded-full ${
+                  isBedVisible ? " bg-jobloop-primary-orange " : " bg-gray-200"
+                }`}
+              >
+                <div
+                  className={`w-2 h-2 rounded-full ${
+                    isBedVisible ? " bg-white " : " bg-transparent"
+                  }`}
+                >
+                  {" "}
+                </div>
+              </div>
+              <p>{titleBed}</p>
+            </button>
+          </div>
 
-                    <div>
-                          <button onClick={handleVirkToggle} className={`md:p-7 flex flex-row gap-3 items-start md:items-center rounded-3xl lg:w-44 w-36 h-7 lg:h-10 border-solid border-black border-2  transition-all duration-600 text-sm 
-                          ${isVirkVisible ? 'border-jobloop-primary-orange border-none md:border-solid  md:border-2'  : ' border-none'}`}>
-                          <div className={ ` flex items-center justify-center w-4 h-4 md:w-7 md:h-7 rounded-full ${isVirkVisible ?' bg-jobloop-primary-orange ': ' bg-gray-200'}`}>
-                                <div className={`w-2 h-2 rounded-full ${isVirkVisible ?' bg-white ': ' bg-transparent'}`} > </div>
-                          </div>
-                          <p>{titleVirk}</p>
-                          </button>
-                    </div>
+          <div>
+            <button
+              onClick={handleVirkToggle}
+              className={`md:p-7 flex flex-row gap-3 items-start md:items-center rounded-3xl lg:w-44 w-36 h-7 lg:h-10 border-solid border-black border-2  transition-all duration-600 text-sm 
+                          ${
+                            isVirkVisible
+                              ? "border-jobloop-primary-orange border-none md:border-solid  md:border-2"
+                              : " border-none"
+                          }`}
+            >
+              <div
+                className={` flex items-center justify-center w-4 h-4 md:w-7 md:h-7 rounded-full ${
+                  isVirkVisible ? " bg-jobloop-primary-orange " : " bg-gray-200"
+                }`}
+              >
+                <div
+                  className={`w-2 h-2 rounded-full ${
+                    isVirkVisible ? " bg-white " : " bg-transparent"
+                  }`}
+                >
+                  {" "}
+                </div>
+              </div>
+              <p>{titleVirk}</p>
+            </button>
           </div>
         </div>
-    
-        <div className="flex justify-center w-full">
-            <div>
-            {isDelVisible && <FaqDeltager id={idOne} contentQ={contentQuestOne}  contentS={contentSvarOne}/>}
-            </div>
-
-            
-            <div>
-            {isBedVisible && <FaqBedrift id={idTwo} contentQ={contentQuestTwo}  contentS={contentSvarTwo} />}
-            </div>
-
-          
-        
-
-            <div>
-            {isVirkVisible && <FaqVirk id={idThree} contentQ={contentQuestThree}  contentS={contentSvarThree} />}
-            </div>
       </div>
-  </div>
 
-      );
+      <div className="flex justify-center w-full">
+        <div>
+          {isDelVisible && (
+            <FaqDeltager id={idOne} contentQ={contentQuestOne} contentS={contentSvarOne} />
+          )}
+        </div>
+
+        <div>
+          {isBedVisible && (
+            <FaqBedrift id={idTwo} contentQ={contentQuestTwo} contentS={contentSvarTwo} />
+          )}
+        </div>
+
+        <div>
+          {isVirkVisible && (
+            <FaqVirk id={idThree} contentQ={contentQuestThree} contentS={contentSvarThree} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
-
