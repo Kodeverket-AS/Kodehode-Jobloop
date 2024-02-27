50237779eb612a76ac08241f9ffc532622512895
KristofferG <kristofferb04.kodehode@gmail.com>
2024-02-27 08:53:17 +0100

    +imgAlt & title for use in logo alt

--- components/Vision2Part.jsx
+++ components/Vision2Part.jsx
@@ -2,7 +2,16 @@ import Image from "next/image";
 import { ExternalLinkButton } from "./Buttons";
 import { PortableText } from "@portabletext/react";
 
-export default function Vision2Part({ content, link, linkText, linkAria, btnBG, imgSide }) {
+export default function Vision2Part({
+  content,
+  link,
+  linkText,
+  linkAria,
+  btnBG,
+  imgSide,
+  imgAlt,
+  title,
+}) {
   return (
     <div
       key={content._id}
@@ -16,7 +25,13 @@ export default function Vision2Part({ content, link, linkText, linkAria, btnBG, 
         } items-center w-5/6 gap-40 ${imgSide == "left" ? "lg:flex-row" : "lg:flex-row-reverse"} `}
       >
         <div className="hidden w-5/6 mt-20 lg:block">
-          <Image width={500} height={500} className="w-full" src={content.ImageTwo}></Image>
+          <Image
+            width={500}
+            height={500}
+            className="w-full"
+            src={content.ImageTwo}
+            alt={imgAlt}
+          ></Image>
         </div>
         <div className="flex flex-col w-5/6 gap-10">
           <Image
@@ -24,6 +39,7 @@ export default function Vision2Part({ content, link, linkText, linkAria, btnBG, 
             height={100}
             className="w-5/6 mx-auto md:w-3/6"
             src={content.ImageOne}
+            alt={`${title} logo`}
           ></Image>
           <PortableText value={content.content} />
           <ExternalLinkButton
