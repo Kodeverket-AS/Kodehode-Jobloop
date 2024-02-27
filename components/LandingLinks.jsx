b8f7c94ffca19d97b21631043b410d67259d0f88
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-02-27 10:39:33 +0100

    updating link section

--- components/LandingLinks.jsx
+++ components/LandingLinks.jsx
@@ -9,54 +9,58 @@ import { LinkButton } from "./Buttons";
 
 export default function LandingLinks() {
   return (
-    <>
-      <div className="flex flex-col px-8 pt-20">
+      <div className="flex flex-col px-8 pt-20 sm:px-10 md:px-12 lg:px-24">
         <div className="flex justify-center w-full">
           <h2 className="text-center">Hvordan kan vi hjelpe deg?</h2>
         </div>
-        <div className="flex justify-center py-20">
-          <div className="flex flex-col gap-12 md:flex-row md:gap-4">
-            <div className="flex flex-col items-center gap-8 md:w-60">
+        <div className="flex justify-center w-full pt-10 pb-20">
+          <div className="flex flex-col gap-20 md:flex-row md:gap-0">
+            <div className="flex flex-col items-center gap-4 px-0 md:w-52 md:items-start md:text-left md:pr-5 lg:pr-5">
               <Image
                 src={koffert2}
                 width={500}
                 height={300}
                 className="w-auto h-20"
               />
-              <h5 className="text-xl font-bold text-center">Arbeidsgiver</h5>
-              <p className="text-center">
+              <h5 className="text-xl font-bold text-center md:text-left">
+                Arbeidsgiver
+              </h5>
+              <p className="text-center md:text-left">
                 For bedrifter som er interessert i utviklere
               </p>
               <div>
                 <LinkButton Path={"/arbeidsgiver"} Text={"Se mer"} />
               </div>
             </div>
-            <div className="flex flex-col items-center gap-8 md:w-60">
+            <div className="flex flex-col items-center gap-4 px-0 md:w-52 md:items-start md:text-left md:px-5 lg:px-5">
               <Image
                 src={hatt}
                 width={500}
                 height={300}
                 className="w-auto h-20"
               />
-              <h5 className="text-xl font-bold text-center ">Deltager</h5>
-              <p className="text-center">For deg som vil delta på Kodehode</p>
+              <h5 className="text-xl font-bold text-center md:text-left ">
+                Deltager
+              </h5>
+              <p className="text-center md:text-left">
+                For deg som vil delta på Kodehode
+              </p>
               <div>
                 <LinkButton Path={"/deltaker"} Text={"Se mer"} />
               </div>
             </div>
-            <div className="flex flex-col items-center gap-8 md:w-60">
+            <div className="flex flex-col items-center gap-4 px-0 md:w-52 md:items-start md:text-left md:pl-5 lg:pl-5">
               <Image
                 src={mappe}
                 width={500}
                 height={300}
                 className="w-auto h-20"
               />
-              <h5 className="text-xl font-bold text-center">
+              <h5 className="text-xl font-bold text-center md:text-left">
                 Samarbeidspartner
               </h5>
-              <p className="text-center">
-                For andre virksomheter som vil
-                samarbeide
+              <p className="text-center md:text-left">
+                For andre virksomheter som vil samarbeide
               </p>
               <div>
                 <LinkButton Path={"/samarbeidspartner"} Text={"Se mer"} />
@@ -65,6 +69,5 @@ export default function LandingLinks() {
           </div>
         </div>
       </div>
-    </>
   );
 }
