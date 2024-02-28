2f0ed852e00e3e104f823a4510f99153c6eb628e
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-02-28 10:14:46 +0100

    switched to sanity data

--- components/LandingVisjon.jsx
+++ components/LandingVisjon.jsx
@@ -2,28 +2,19 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import visjonlanding2 from "../public/visjonlanding2.jpeg";
 import { LinkButton } from "./Buttons";
 import { PortableText } from "@portabletext/react";
 
-export default function LandingVisjon() {
+export default function LandingVisjon({ content }) {
   return (
     <>
       <div className="flex flex-col justify-center gap-10 px-8 py-20 md:flex-row lg:px-24">
         <div className="w-full md:w-1/2">
-          <Image src={visjonlanding2} width={500} height={300} className="rounded" />
+          <Image src={content.image} width={500} height={300} className="rounded" />
         </div>
         <div className="flex flex-col gap-4 md:w-1/2">
-          <h2>Inkludering lønner seg.</h2>
-          <p>
-            Andelen unge i utenforskap med interesse for gaming og IT i Norge
-            øker, det samme gjør behovet for IT-talent og utvikler i fremtiden.
-            Vår visjon er å skape verdi for samfunn og næringsliv ved å redusere
-            utenforskap gjennom lønnsom opplæring, rekruttering, matchmaking og
-            onboarding innen IT-faget. Vi kjemper for et mer inkluderende
-            samfunn, og å skinne lys på at inkludering ikke behøver å være en
-            utgift, men en verdi for bedrifter såvel som samfunnet som helhet.
-          </p>
+          <h2>{content.title}</h2>
+          <PortableText value={content.content} />
           <div className="pt-5 mx-auto md:mx-0">
             <LinkButton Path={"/visjon"} Text={"Se mer"} />
           </div>
