3dac8fc2d3616eae27888d40cd3f7c9e38c2c41b
KristofferG <kristofferb04.kodehode@gmail.com>
2024-02-29 13:48:01 +0100

    - some bottom padding

--- src/app/visjon/page.js
+++ src/app/visjon/page.js
@@ -28,7 +28,7 @@ export default async function Vision() {
             />
             <div className="flex flex-col items-center justify-center mt-[500px] md:mt-0">
               <div className="w-full bg-jobloop-primary-orange mt-14 md:mt-0">
-                <h2 className="p-20 text-2xl font-extrabold text-center text-white md:text-3xl">
+                <h2 className="p-20 text-2xl font-extrabold text-center text-white pb-7 md:text-3xl">
                   Samarbeidsprosjekter
                 </h2>
               </div>
