33991b95c5169f78c2ac7daed3f8d152504d569f
steiningekodehode <steininge.kodehode@gmail.com>
2024-03-04 10:03:45 +0100

    minor changes

--- components/OmOssTop.jsx
+++ components/OmOssTop.jsx
@@ -7,28 +7,27 @@ import { LinkButton } from "./Buttons";
 export default function OmOssTop({ content, buttonText }) {
    return (
     <>
-    <div className="flex items-center justify-start w-full md:flex-row">
-    
-    <div className="flex items-center justify-start h-96 w-70 md:w-1/2" >
-          <Image
-            src={content.image}
-            width={561}
-            height={820}
-            className="object-cover w-full h-full"
-            alt="Picture of empoyees"
-          />
-          </div>
-          <div className="w-full p-8 space-y-4 bg-white md:w-1/2">
-          <h1 className="text-xl text-jobloop-primary-green md:text-2xl lg:text-3xl">{content.title}</h1>
-            <PortableText value={content.content} />
-        
-        <div className="hidden mt-10 md:block">
-            <LinkButton Path="/contact" Text={buttonText} />
-            </div>
-          </div>
-            </div>
-    
-    </>
+    <div className="flex flex-row items-start justify-start w-full my-20 md:justify-center">
+  <div className="w-full gap-y-20 md:w-1/2 lg:w-2/6">
+    <div className="pb-[150%] relative"> {/* Adjust the padding-bottom value to control aspect ratio */}
+      <Image
+        src={content.image}
+        width={561}
+        height={820}
+        className="absolute top-0 left-0 object-cover w-full h-full"
+        alt="Picture of employees"
+      />
+    </div>
+  </div>
+  <div className="w-full pb-8 pr-8 pl- md:w-1/2 lg:w-2/6">
+    <h1 className="mb-10 text-5xl top text-jobloop-primary-green">{content.title}</h1>
+    <PortableText value={content.content} />
+    <div className="hidden mt-20 md:block">
+      <LinkButton Path="/contact" Text={buttonText} />
+    </div>
+  </div>
+</div>
+</>
    ); 
 }
 
