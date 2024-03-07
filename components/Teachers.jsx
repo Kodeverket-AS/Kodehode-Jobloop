aae487bd3c7b732679c95a383f69c300806dd3ea
KristofferG <kristofferb04.kodehode@gmail.com>
2024-03-07 14:23:47 +0100

    testtest

--- components/Teachers.jsx
+++ components/Teachers.jsx
@@ -12,7 +12,7 @@ export default function Teachers({ content, title }) {
         <div className="flex flex-row w-full pb-10 mx-auto md:flex-col md:w-28 md:pb-0 md:mx-0 lg:w-36">
           <div className="w-1/2 pr-3 md:mb-2 md:h-[150px] md:w-full h-[170px] md:pr-0">
             <Image
-              src={urlFor(content[0].image).fit("min").width(300).height(300).url()}
+              src={urlFor(content[0].image).width(300).height(300).url()}
               width={300}
               height={300}
               className="relative object-cover w-full h-full border border-white rounded"
@@ -27,7 +27,7 @@ export default function Teachers({ content, title }) {
         <div className="flex flex-row w-full pb-10 mx-auto md:flex-col md:w-28 md:pb-0 md:mx-0 lg:w-36">
           <div className="w-1/2 pr-3 md:mb-2 md:h-[150px] md:w-full h-[170px] md:pr-0">
             <Image
-              src={urlFor(content[1].image).fit("min").width(300).height(300).url()}
+              src={urlFor(content[1].image).width(300).height(300).url()}
               width={300}
               height={300}
               className="object-cover w-full h-full border border-white rounded"
@@ -42,7 +42,7 @@ export default function Teachers({ content, title }) {
         <div className="flex flex-row w-full pb-10 mx-auto md:flex-col md:w-28 md:pb-0 md:mx-0 lg:w-36">
           <div className="w-1/2 pr-3 md:mb-2 md:h-[150px] md:w-full h-[170px] md:pr-0">
             <Image
-              src={urlFor(content[2].image).fit("min").width(300).height(300).url()}
+              src={urlFor(content[2].image).width(300).height(300).url()}
               width={300}
               height={300}
               className="object-cover w-full h-full border border-white rounded"
@@ -57,7 +57,7 @@ export default function Teachers({ content, title }) {
         <div className="flex flex-row w-full pb-10 mx-auto md:flex-col md:w-28 md:pb-0 md:mx-0 lg:w-36">
           <div className="w-1/2 pr-3 md:mb-2 md:h-[150px] md:w-full h-[170px] md:pr-0">
             <Image
-              src={urlFor(content[3].image).fit("min").width(300).height(300).url()}
+              src={urlFor(content[3].image).width(300).height(300).url()}
               width={300}
               height={300}
               className="object-cover w-full h-full border border-white rounded"
