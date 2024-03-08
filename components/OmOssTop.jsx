d6c8be746c89fbaad88c8c7cdb95223cbf0fcc1e
steiningekodehode <steininge.kodehode@gmail.com>
2024-03-08 12:44:42 +0100

    minor changes

--- components/OmOssTop.jsx
+++ components/OmOssTop.jsx
@@ -5,29 +5,29 @@ import { PortableText } from "@portabletext/react";
 import { LinkButton } from "./Buttons";
 
 export default function OmOssTop({ content, buttonText }) {
-   return (
-    <>
-    <div className="flex flex-row w-full my-24 place-content-evenly">
-  <div className="w-full md:w-1/2 lg:w-2/6">
-    <div className="pb-[150%] relative"> {/* Adjust the padding-bottom value to control aspect ratio */}
-      <Image
-        src={content.image}
-        width={561}
-        height={820}
-        className="absolute top-0 left-0 object-cover w-full h-full"
-        alt="Picture of employees"
-      />
+  return (
+    <div className="flex items-center justify-center w-full px-4 py-20 md:px-12 lg:px-24 md:flex-nowrap">
+      {/* Image container */}
+      <div className="flex-initial mx-auto w-36 h-52 md:w-40 md:h-48 md:mx-0">
+        <Image
+          src={content.image}
+          alt="Picture of employees"
+          width={561}
+          height={820}
+          className="object-cover w-full h-full"
+        />
+      </div>
+
+      {/* Text container */}
+      <div className="w-full px-4 py-8 md:w-1/2 lg:w-2/5 md:py-0 md:pl-8">
+        <h1 className="text-3xl font-semibold text-jobloop-primary-green">{content.title}</h1>
+        <PortableText value={content.content} />
+        <div className="mt-8">
+          <LinkButton Path="/contact" Text={buttonText} />
+        </div>
+      </div>
     </div>
-  </div>
-  <div className="w-full pb-8 pr-8 pl- md:w-1/2 lg:w-2/6">
-    <h1 className="mb-10 text-6xl text-jobloop-primary-green">{content.title}</h1>
-    <PortableText value={content.content} />
-    <div className="hidden mt-20 md:block">
-      <LinkButton Path="/contact" Text={buttonText} />
-    </div>
-  </div>
-</div>
-</>
-   ); 
+  );
 }
+
 
