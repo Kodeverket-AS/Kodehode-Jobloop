5200e1b01450acc7d6bcc48d77fb619d7211f121
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-03-08 12:00:45 +0100

    revised fourIconRow

--- components/FourIconRow.jsx
+++ components/FourIconRow.jsx
@@ -5,15 +5,11 @@ import Image from "next/image";
 
 export default function FourIconRow({ title, content, alignment }) {
   return (
-    <div className="flex flex-col items-center w-full p-8 text-center text-white bg-jobloop-primary-green sm:p-10 md:p-12 md:w-screen lg:p-24">
+    <div className="flex flex-col items-center w-full px-8 py-20 text-center text-white bg-jobloop-primary-green sm:px-10 md:px-12 md:w-screen lg:px-24">
       <h2 className="pb-10 md:pb-20">{title}</h2>
-      <div className="flex flex-col w-full md:flex-row md:justify-center">
-        <div className="flex flex-row justify-between mb-5 md:mb-0">
-          <div
-            className={`flex flex-col ${
-              alignment === "left" ? "items-start" : "items-center"
-            } mr-5 w-28 md:mr-3 md:w-36 2xs:w-32 xs:w-36 sm:w-56 md:mx-0 lg:mr-7 lg:w-40 xl:mr-10 xl:w-44`}
-          >
+      <div className="flex flex-col w-full md:flex-row md:justify-between lg:gap-16 xl:gap-32 xl:max-w-[1088px]">
+        <div className="flex flex-row justify-between mb-5 md:mb-0 md:justify-evenly md:w-1/2 lg:gap-16 xl:gap-32">
+          <div className={`flex flex-col ${alignment === "left" ? "items-start" : "items-center"} mr-5 w-28 md:w-36 2xs:w-32 xs:w-36 sm:w-56 md:mr-0 lg:w-40 xl:w-44`}>
             <Image
               src={content[0].logo}
               width={300}
@@ -28,11 +24,7 @@ export default function FourIconRow({ title, content, alignment }) {
               {content[0].content}
             </p>
           </div>
-          <div
-            className={`flex flex-col ${
-              alignment === "left" ? "items-start" : "items-center"
-            } mr-5 w-28 md:mr-3 md:w-36 2xs:w-32 xs:w-36 sm:w-56 md:mx-0 lg:mr-7 lg:w-40 xl:mr-10 xl:w-44`}
-          >
+          <div className={`flex flex-col ${alignment === "left" ? "items-start" : "items-center"} mr-5 w-28 md:mr-0 md:w-36 2xs:w-32 xs:w-36 sm:w-56 lg:w-40 xl:w-44`}>
             <Image
               src={content[1].logo}
               width={300}
@@ -48,12 +40,8 @@ export default function FourIconRow({ title, content, alignment }) {
             </p>
           </div>
         </div>
-        <div className="flex flex-row justify-between">
-          <div
-            className={`flex flex-col ${
-              alignment === "left" ? "items-start" : "items-center"
-            } mr-5 w-28 md:mr-3 md:w-36 2xs:w-32 xs:w-36 sm:w-56 md:mx-0 lg:mr-7 lg:w-40 xl:mr-10 xl:w-44`}
-          >
+        <div className="flex flex-row justify-between md:justify-evenly md:w-1/2 lg:gap-16 xl:gap-32">
+          <div className={`flex flex-col ${alignment === "left" ? "items-start" : "items-center"} mr-5 w-28 md:mr-0 md:w-36 2xs:w-32 xs:w-36 sm:w-56 lg:w-40 xl:w-44`}>
             <Image
               src={content[2].logo}
               width={300}
@@ -68,11 +56,7 @@ export default function FourIconRow({ title, content, alignment }) {
               {content[2].content}
             </p>
           </div>
-          <div
-            className={`flex flex-col ${
-              alignment === "left" ? "items-start" : "items-center"
-            } mr-5 w-28 md:mr-3 md:w-36 2xs:w-32 xs:w-36 sm:w-56 md:mx-0 lg:mr-7 lg:w-40 xl:mr-10 xl:w-44`}
-          >
+          <div className={`flex flex-col ${alignment === "left" ? "items-start" : "items-center"} mr-5 w-28 md:mr-0 md:w-36 2xs:w-32 xs:w-36 sm:w-56 lg:w-40 xl:w-44`}>
             <Image
               src={content[3].logo}
               width={300}
