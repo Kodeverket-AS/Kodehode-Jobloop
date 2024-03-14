154f8a97164ec554043b768cc2356d8325602543
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-03-14 13:50:44 +0100

    landing page fixed

--- components/CompanyCarousel.jsx
+++ components/CompanyCarousel.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function CompanyCarousel({ content }) {
   return (
-    <div className="flex flex-col w-screen py-20">
+    <div className="flex flex-col w-screen py-20 2xl:max-w-[1600px]">
       <h2 className="text-lg font-semibold text-center">Noen av våre praksisbedrifter</h2>
       <div className="grid grid-flow-col py-5 overflow-hidden ">
         <div className="grid grid-flow-col animate-scroll">
