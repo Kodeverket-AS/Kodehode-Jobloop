22e70577ecbc7238c2c9df14f84441950587090e
KristofferG <kristofferb04.kodehode@gmail.com>
2024-03-14 10:02:34 +0100

    add meta titles for each page, clean up layout

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,41 +4,37 @@ import Offices from "../../../components/Offices";
 import Teachers from "../../../components/Teachers";
 import JobloopPodium from "../../../components/JobloopPodium";
 
+export const metadata = {
+  title:
+    "Om oss: en alternativ plattform for kvalifisering og rekruttering av junior utviklere og innholdsprodusenter | Kodehode",
+};
 
 export default async function About() {
- 
   const data = await getData();
   const heroSubData = data.heroSub[7];
-  const selectedTeachersData = [data.personDatabase[0], data.personDatabase[1], data.personDatabase[6], data.personDatabase[7]];
+  const selectedTeachersData = [
+    data.personDatabase[0],
+    data.personDatabase[1],
+    data.personDatabase[6],
+    data.personDatabase[7],
+  ];
   console.log(data.about);
-return (
+  return (
     <>
       <main className="flex flex-col items-center min-h-screen p-0">
-      <HeroSub 
-       content={heroSubData}
-       buttonText="Kontakt oss"
-       key={heroSubData._id} />
+        <HeroSub content={heroSubData} buttonText="Kontakt oss" key={heroSubData._id} />
 
-      <div className="flex flex-col items-center w-full mt-5">
+        <div className="flex flex-col items-center w-full mt-5">
+          <div className="w-full">
+            <Teachers content={selectedTeachersData} />
+          </div>
+        </div>
+        <div className="flex flex-col items-center ">
+          <JobloopPodium content={data.about} />
+        </div>
         <div className="w-full">
-        <Teachers
-        content={selectedTeachersData} 
-        />
+          <Offices content={data.OurLocals} />
         </div>
-         </div>
-      <div className="flex flex-col items-center ">
-        <JobloopPodium content={data.about}/>
-        </div>
-      <div className="w-full">
-        <Offices
-        content={data.OurLocals} 
-        />
-      </div>
-
-         
-         
-
-      
       </main>
     </>
   );
