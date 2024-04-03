119eb35a4915da314a647da09343f7b0cd927c17
KristofferG <kristofferb04.kodehode@gmail.com>
2024-04-03 13:18:08 +0200

    +VisionTop, based on OmOssTop, swapped VisionSub out with VisionTop

--- components/VisionSub.jsx
+++ components/VisionSub.jsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { PortableText } from "@portabletext/react";
 
-export default function VisionSub({ content }) {
+export function VisionSub({ content }) {
   return (
     <>
       <div className="absolute top-0 block w-full -z-10 md:hidden">
@@ -44,5 +44,29 @@ export default function VisionSub({ content }) {
         </div>
       </div>
     </>
+  );
+}
+
+export default function VisionTop({ content }) {
+  return (
+    <div className="flex flex-col items-center justify-start w-full py-20 md:px-12 lg:px-24 md:flex-row md:items-start md:justify-between md:w-screen 2xl:max-w-[1600px]">
+      <div className="flex w-screen h-auto p-0 mx-auto md:pr-5 md:mx-0 md:w-[20rem] md:h-[37rem] lg:w-[24rem] 2xl:w-[32rem]">
+        <Image
+          src={content.image}
+          alt={content.alt || ""}
+          width={1000}
+          height={1000}
+          className="object-cover w-full h-full"
+        />
+      </div>
+
+      {/* Text container */}
+      <div className="flex flex-col w-full px-8 sm:px-10 sm:my-10 md:w-1/2 md:my-0 md:px-0">
+        <h1 className="py-10 text-start text-jobloop-primary-green md:pb-10 md:pt-0">
+          {content.title}
+        </h1>
+        <PortableText value={content.content} />
+      </div>
+    </div>
   );
 }
