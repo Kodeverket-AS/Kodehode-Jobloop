b6a81f8f4dd6a2c91f8589e20621e8e7ac57ed47
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-04-10 09:50:13 +0200

    fixed landing page

--- components/LandingLinks.jsx
+++ components/LandingLinks.jsx
@@ -14,26 +14,46 @@ export default function LandingLinks() {
         <h2 className="text-center">Hvordan kan vi hjelpe deg?</h2>
       </div>
       <div className="flex flex-col w-full gap-20 pt-10 pb-20 justify-evenly md:flex-row md:gap-0 xl:max-w-[800px] xl:justify-between xl:mx-auto">
-        <div className="flex flex-col items-center gap-4 px-0 md:w-52 md:items-start md:text-left md:pr-5 lg:pr-5">
-          <Image src={koffert} width={500} height={300} className="w-auto h-20" alt="" />
-          <h5 className="text-xl font-bold text-center md:text-left">Arbeidsgiver</h5>
-          <p className="text-center md:text-left">For bedrifter som er interessert i utviklere</p>
+        <div className="flex flex-col items-start w-40 gap-4 px-0 mx-auto text-left md:w-52 md:pr-5 lg:pr-5 md:mx-0">
+          <Image
+            src={koffert}
+            width={500}
+            height={300}
+            className="w-auto h-24 md:h-20"
+            alt=""
+          />
+          <h5 className="text-xl font-bold text-left">Arbeidsgiver</h5>
+          <p className="text-left">
+            For bedrifter som er interessert i utviklere
+          </p>
           <div>
             <LinkButton Path={"/arbeidsgiver"} Text={"Les mer"} />
           </div>
         </div>
-        <div className="flex flex-col items-center gap-4 px-0 md:w-52 md:items-start md:text-left md:px-5 lg:px-5">
-          <Image src={hatt} width={500} height={300} alt="" className="w-auto h-20" />
-          <h5 className="text-xl font-bold text-center md:text-left ">Deltager</h5>
-          <p className="text-center md:text-left">For deg som vil delta på Kodehode</p>
+        <div className="flex flex-col items-start w-40 gap-4 px-0 mx-auto text-left md:w-52 md:px-5 lg:px-5 md:mx-0">
+          <Image
+            src={hatt}
+            width={500}
+            height={300}
+            alt=""
+            className="w-auto h-24 md:h-20"
+          />
+          <h5 className="text-xl font-boldtext-left ">Deltager</h5>
+          <p className="text-left">For deg som vil delta på Kodehode</p>
           <div>
             <LinkButton Path={"/deltaker"} Text={"Les mer"} />
           </div>
         </div>
-        <div className="flex flex-col items-center gap-4 px-0 md:w-52 md:items-start md:text-left md:pl-5 lg:pl-5">
-          <Image src={dokument} width={500} height={300} alt="" className="w-auto h-20" />
-          <h5 className="text-xl font-bold text-center md:text-left">Samarbeidspartner</h5>
-          <p className="text-center md:text-left">For andre virksomheter som vil samarbeide</p>
+        <div className="flex flex-col items-start w-40 gap-4 px-0 mx-auto text-left md:w-52 md:pl-5 lg:pl-5 md:mx-0">
+          <Image
+            src={dokument}
+            width={500}
+            height={300}
+            alt=""
+            className="w-auto h-24 md:h-20"
+          />
+          <h5 className="text-xl font-bold text-left">Samarbeidspartner</h5>
+          <p className="text-left">For andre virksomheter som vil samarbeide</p>
           <div>
             <LinkButton Path={"/samarbeidspartner"} Text={"Les mer"} />
           </div>
