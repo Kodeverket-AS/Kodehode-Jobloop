86d63744f9c9c6b89e00065a728e635ec5fee3f2
KristofferG <kristofferb04.kodehode@gmail.com>
2024-04-10 13:52:12 +0200

    tweak sizing

--- components/Offices.jsx
+++ components/Offices.jsx
@@ -6,21 +6,21 @@ import Image from "next/image";
 export default function Offices({ content }) {
   return (
     <div className="flex flex-col items-center w-full px-8 py-10 md:w-full sm:px-10 md:px-12 lg:px-24">
-      <h2 className="pb-10">Våre lokaler</h2>
-      <div className="w-full pt-0 gap-2 pb-10 md:flex md:justify-center xl:max-w-[1088px]">
+      <h2 className="pb-10 z200:text-jobloop-primary-green">Våre lokaler</h2>
+      <div className="w-full pt-0 gap-8 pb-10 md:flex-wrap md:flex md:justify-center xl:max-w-[1600px]">
         {content.map((offices, index) => (
           <div
             key={offices.id + index}
-            className="flex flex-row justify-between w-full pb-10 mx-auto 2xs:mx-0 md:flex-col md:w-40 lg:w-48 md:pb-0"
+            className="flex flex-row justify-between w-full h-full pb-10 mx-auto 2xs:mx-0 md:flex-col md:w-40 lg:w-48 md:pb-0"
           >
             <Image
               src={offices.image}
               width={1000}
               height={1000}
-              className="object-cover w-24 h-full mr-3 border rounded border-kv-white 2xs:w-1/2 md:mb-2 md:w-full md:mr-0"
+              className="object-cover w-24 h-full mr-3 border rounded border-kv-white 2xs:portrait:w-1/2 md:mb-2 md:w-full md:portrait:w-full md:mr-0"
               alt={offices.alt || ""}
             />
-            <div className="w-40 text-left xs:w-1/2 md:w-full">
+            <div className="w-40 text-left sm:w-full xs:w-1/2 md:w-full">
               <h2>{offices.title}</h2>
               <p>{offices.adresse}</p>
               <p>{offices.postnr}</p>
