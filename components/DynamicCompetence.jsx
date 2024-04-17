e1dbc00d5cd4d77caeaaad9b99785d6820cbcd6b
KristofferG <kristofferb04.kodehode@gmail.com>
2024-04-17 12:05:31 +0200

    fix icon sizing and spacing

--- components/DynamicCompetence.jsx
+++ components/DynamicCompetence.jsx
@@ -5,17 +5,14 @@ import Image from "next/image";
 export default function DynamicCompetence({ content }) {
   return (
     <div className="flex flex-col items-start w-full min-h-full pb-20 text-left md:w-screen lg:px-24 md:items-center">
-      <div className="grid grid-cols-2 gap-y-40 md:gap-y-48 gap-x-8 md:grid-cols-3 align-center">
+      <div className="grid w-full h-full grid-cols-2 gap-y-20 md:gap-y-24 gap-x-8 md:grid-cols-3 align-center">
         {content.map((item, index) => (
-          <div
-            key={item.id + index}
-            className="flex flex-col items-start h-16 m-5 w-28 md:w-36 2xs:w-32 xs:w-36 sm:w-56 md:mr-0 lg:w-40 xl:w-44"
-          >
+          <div key={item.id + index} className="flex flex-col items-start w-full h-full">
             <Image
               src={item.logo}
               width={300}
               height={300}
-              className="w-16 mb-2 md:w-32"
+              className="w-16 h-16 mb-2 md:w-32 md:h-32"
               alt={item.alt || ""}
             />
             <h3 className="text-left">{item.title}</h3>
