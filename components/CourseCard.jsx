027c926d90d4722fe62a8ee3dc5e5f70b4016ebd
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-04-29 13:02:49 +0200

    increased space on desktop

--- components/CourseCard.jsx
+++ components/CourseCard.jsx
@@ -7,16 +7,16 @@ export default function CourseCard({ content }) {
   return (
     <div className="flex flex-col px-5 py-3 shadow-md md:py-5 bg-kv-white">
       <h3>{content.title}</h3>
-      <p className="pb-3 border-b-4 border-kv-black">{content.subheader}</p>
-      <h4 className="pt-3">Forutsetninger</h4>
+      <p className="pb-3 border-b-4 border-kv-black md:pt-5">{content.subheader}</p>
+      <h4 className="pt-3 md:pt-5">Forutsetninger</h4>
       <p>{content.expectations}</p>
-      <h4 className="pt-3">Tidsplan</h4>
+      <h4 className="pt-3 md:pt-5">Tidsplan</h4>
       <p>{content.schedule}</p>
-      <h4 className="pt-3">Undervisningsform</h4>
+      <h4 className="pt-3 md:pt-5">Undervisningsform</h4>
       <p>{content.educationStyle}</p>
-      <h4 className="pt-3">Jobbmarked</h4>
+      <h4 className="pt-3 md:pt-5">Jobbmarked</h4>
       <p>{content.jobMarket}</p>
-      <h4 className="pt-3">Sted</h4>
+      <h4 className="pt-3 md:pt-5">Sted</h4>
       <p>{content.place}</p>
     </div>
   );
--- components/InternshipCard.jsx
+++ components/InternshipCard.jsx
@@ -7,16 +7,16 @@ export default function InternshipCard({ content }) {
   return (
     <div className="flex flex-col px-5 py-3 shadow-md md:py-5 bg-kv-white">
       <h3>{content.title}</h3>
-      <p className="pb-3 border-b-4 border-kv-black">{content.subheader}</p>
-      <h4 className="pt-3">Forutsetninger</h4>
+      <p className="pb-3 border-b-4 border-kv-black md:pt-5">{content.subheader}</p>
+      <h4 className="pt-3 md:pt-5">Forutsetninger</h4>
       <p>{content.expectations}</p>
-      <h4 className="pt-3">Tidsplan</h4>
+      <h4 className="pt-3 md:pt-5">Tidsplan</h4>
       <p>{content.schedule}</p>
-      <h4 className="pt-3">Veiledning</h4>
+      <h4 className="pt-3 md:pt-5">Veiledning</h4>
       <p>{content.guidance}</p>
-      <h4 className="pt-3">Tilbud om fast stilling?</h4>
+      <h4 className="pt-3 md:pt-5">Tilbud om fast stilling?</h4>
       <p>{content.jobOffer}</p>
-      <h4 className="pt-3">Hjelp til å bli godkjent lærlingbedrift</h4>
+      <h4 className="pt-3 md:pt-5">Hjelp til å bli godkjent lærlingbedrift</h4>
       <p>{content.help}</p>
     </div>
   );
