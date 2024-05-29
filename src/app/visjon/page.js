283f10804c0c48da4aef91a15e813f323a64a6d5
KristofferG <kristofferb04.kodehode@gmail.com>
2024-05-29 09:19:02 +0200

    adjust line placement and width

--- src/app/visjon/page.js
+++ src/app/visjon/page.js
@@ -19,11 +19,11 @@ export default async function Vision() {
       <main className="flex flex-col items-center w-full min-h-screen">
         <VisionTop content={data.heroSub[3]} key={data.heroSub[3]._id} />
         <div className="flex flex-col items-center justify-center md:mt-0">
-          <div className="w-full">
+          <div className="w-fit">
             <h2 className="p-20 text-2xl font-extrabold text-center text-kv-black pb-7 md:text-3xl">
               Samarbeidsprosjekter
             </h2>
-            <hr className="w-1/3 h-2 bg-jobloop-secondary-orange ml-[33%] mt-2" />
+            <hr className="w-[62%] ml-[19%] h-2 bg-jobloop-secondary-orange" />
           </div>
           <Vision2Part
             content={data.VisionSections[1]}
@@ -45,11 +45,11 @@ export default async function Vision() {
           {/* <div className="block md:hidden">
             <ProjectScroll content={data.VisionSections}></ProjectScroll>
           </div> */}
-          <div>
+          <div className="w-fit">
             <h1 className="text-2xl font-extrabold text-center md:text-4xl ">
               Hva partnere sier om oss
             </h1>
-            <hr className="w-1/3 h-2 bg-jobloop-secondary-orange ml-[33%] mt-2" />
+            <hr className="w-full h-2 mt-4 bg-jobloop-secondary-orange" />
           </div>
           <div className="flex justify-center h-fit max-w-[1600px] min-h-fit">
             <EndorsementComp data={data.endorsement} />
