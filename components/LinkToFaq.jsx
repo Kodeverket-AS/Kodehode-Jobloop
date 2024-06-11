3883d50c554fb8233aed2131ea40c902cb4b2681
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-06-11 12:32:35 +0200

    update - simplified code. added styling based on design. basics

--- components/LinkToFaq.jsx
+++ components/LinkToFaq.jsx
@@ -9,13 +9,24 @@ export default function LinkToFaq() {
   return (
     <div className="w-full pt-10 md:pt-20">
       <div className="flex flex-col items-center">
-        <h2 className="mb-10 md:mb-20">Har du flere spørsmål?</h2>
+        <div className="border-b-4 border-jobloop-primary-green pb-4 mb-8">
+          <h1 className="tracking-tight">Har du flere spørsmål?</h1>
+        </div>
         <div className="z-0">
-          <LinkButton Path="/FAQ" Text="Gå til FAQ" />
+          <LinkButton
+            Path="/FAQ"
+            Text="Gå til FAQ"
+          />
         </div>
       </div>
       <div className="flex justify-end w-full md:mt-[-130px] mt-[-70px] sm:mt-[-100px] 2xl:mt-[-233px]">
-        <Image src={Fargefigur} width={1000} height={1000} className="w-1/2 mr-0" alt="" />
+        <Image
+          src={Fargefigur}
+          width={1000}
+          height={1000}
+          className="w-1/2 mr-0"
+          alt=""
+        />
       </div>
     </div>
   );
--- components/Offices.jsx
+++ components/Offices.jsx
@@ -4,23 +4,23 @@ import Image from "next/image";
 
 export default function Offices({ content }) {
   return (
-    <div className="px-8 pt-20 pb-10 text-center md:w-screen sm:px-10 md:px-12 lg:px-24">
-      <div className="border-b-4 border-jobloop-primary-green pb-2 mb-4">
+    <div className="px-8 pt-20 pb-10 text-center md:w-screen md:px-12 lg:px-24">
+      <div className="border-b-4 border-jobloop-primary-green pb-2 mb-6">
         <h2 className="tracking-tight">Våre lokaler</h2>
       </div>
-      <div className="flex flex-wrap gap-8 justify-center md:justify-center">
+      <div className="grid w-full h-full grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-4 align-center">
         {content.map((offices, index) => (
           <div
             key={offices.id + index}
-            className="w-60">
+            className="w-full">
             <Image
               src={offices.image}
               width={500}
               height={500}
-              className="object-cover w-full "
+              className="rounded-md w-full h-40 object-cover"
               alt={offices.alt || ""}
             />
-            <div className="text-left">
+            <div className="text-left pt-1">
               <h3>{offices.title}</h3>
               <p>{offices.adresse}</p>
               <p>{offices.postnr}</p>
