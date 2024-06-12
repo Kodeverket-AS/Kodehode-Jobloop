0d4d42f0896e5ba9f5671d30f826777bd4395fd9
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-06-12 14:56:48 +0200

    update - deltaker page etc updated design and clean up

--- components/Results.jsx
+++ components/Results.jsx
@@ -7,27 +7,30 @@ import { PortableText } from "@portabletext/react";
 export default function Results({ content }) {
   //console.log(content);
   return (
-    <div className="flex flex-col w-full px-8 py-20 sm:px-10 md:px-12 lg:px-24 sm:flex-row 2xl:max-w-[1600px]">
-      <div className="w-full sm:w-1/2">
+    <div className=" flex flex-col w-full gap-12 px-8 py-20 sm:px-10 md:px-12 lg:px-24 sm:flex-row 2xl:max-w-[1600px]">
+      <div className="w-full">
         <Image
           src={content.image}
           width={500}
           height={500}
           alt={content.alt}
-          className="pt-0 pr-0 rounded sm:pr-3 md:pr-10 sm:pt-3"
+          className="w-full rounded"
         />
       </div>
-      <div className="flex flex-col w-full pl-0 pr-0 sm:w-1/2 sm:pl-5">
-        <h2 className="pt-3 pb-5 sm:pt-0">Kodehode gir resultater</h2>
-        <PortableText value={content.content} />
-        <p className="py-5">{content.person}</p>
+      <div className="flex flex-col gap-4 w-full">
+        <div className="border-b-2 pb-2 border-jobloop-primary-orange">
+          <h2>Kodehode gir resultater</h2>
+        </div>
+        <div className="text-lg">
+          <PortableText value={content.content} />
+        </div>
+        <p className="py-5"> - {content.person}</p>
         <a
           href={content.link}
           target="blank"
           rel="noopener noreferrer"
           aria-label="lenke til artikkelen"
-          className="underline text-jobloop-primary-green max-w-fit"
-        >
+          className="underline text-jobloop-primary-green max-w-fit">
           Les mer på Kode24
         </a>
       </div>
--- src/app/deltaker/page.js
+++ src/app/deltaker/page.js
@@ -30,34 +30,51 @@ export default async function Deltaker() {
         key={data.heroSub[2]._id}
       />
       <div className="flex flex-col items-center mt-[26rem] md:mt-20">
-        <KodehodeSummary content1={data.origin[1]} content2={data.courseTypes} />
+        <KodehodeSummary
+          content1={data.origin[1]}
+          content2={data.courseTypes}
+        />
         <StudyInParts content={data.CourseSections} />
-        <div className="w-full bg-jobloop-primary-green">
+        <div className="w-full flex flex-col gap-8 py-12">
+          <div className="border-b-2 pb-2 border-jobloop-primary-orange w-fit mx-auto">
+            <h2 className="text-center">Koding i HTML og CSS</h2>
+          </div>
           <VideoComp1
             url="https://www.youtube.com/embed/LocPs6zdkrQ?si=afaKRzcMA80WvwtW&amp;"
             alt={"Timelapse video av noen som koder en portfolio i HTML og CSS"}
           />
         </div>
-        <MultiCompetence content1={data.relevantCompetence[0]} content2={data.competenceIcons} />
-        <div className="w-full">
+        <MultiCompetence
+          content1={data.relevantCompetence[0]}
+          content2={data.competenceIcons}
+        />
+        <div className="w-full flex flex-col gap-8 pb-12">
+          <div className="border-b-2 pb-2 border-jobloop-primary-orange w-fit mx-auto">
+            <h2 className="text-center">Design i Figma</h2>
+          </div>
           <VideoComp1
             url="https://www.youtube.com/embed/6riX-2lnIyg?si=g3aOkgbSOaBsJE-B&amp;"
             alt={"Timelapse video av noen som designer en nettside i Figma"}
           />
         </div>
-        <Teachers content={teachers} title={"Våre veiledere"} />
+        {/* <Teachers
+          content={teachers}
+          title={"Våre veiledere"}
+        /> */}
         {/*<Platforms />*/}
         {/*<div className=" bg-jobloop-secondary-orange">
           <Results content={data.results[0]} />
         </div>*/}
-        <Results content={data.results[0]} />
+        <div className="bg-neutral-100 w-screen">
+          <Results content={data.results[0]} />
+        </div>
         <ImageGallery content={data.ImageGallery} />
-        <div className="w-full bg-jobloop-primary-green">
+        {/* <div className="w-full h-60">
           <VideoComp1
             url="https://www.youtube.com/embed/gIrgqXD27EI?si=bh2im5jdpu-apCIQ&amp;"
             alt={"Timelapse av noen som koder en app i React Native"}
           />
-        </div>
+        </div> */}
         <LinkToFaq />
       </div>
     </main>
