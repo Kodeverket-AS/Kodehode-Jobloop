22f6e3c3280ee28be9e4cb6450090a56f45ff4ce
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-06-12 14:05:45 +0200

    update - fixed issue on om oss page

--- components/OmOssTop.jsx
+++ components/OmOssTop.jsx
@@ -6,8 +6,8 @@ import { LinkButton } from "./Buttons";
 
 export default function OmOssTop({ content, buttonText }) {
   return (
-    <div className="flex flex-col items-center justify-start w-full py-20 md:px-12 lg:px-24 md:flex-row md:items-start md:justify-between md:w-screen 2xl:max-w-[1600px]">
-      <div className="flex w-screen h-auto p-0 mx-auto md:pr-5 md:mx-0 md:w-[20rem] md:h-[37rem] lg:w-[24rem] 2xl:w-[32rem]">
+    <div className="flex flex-col justify-start w-full py-20 md:px-12 lg:px-24 md:flex-row md:items-start md:justify-between 2xl:max-w-[1600px]">
+      <div className="flex w-full h-auto ">
         <Image
           src={content.image}
           alt="Picture of employees"
@@ -18,11 +18,11 @@ export default function OmOssTop({ content, buttonText }) {
       </div>
 
       {/* Text container */}
-      <div className="flex flex-col w-full px-8 sm:my-10 md:w-1/2 md:my-0 ">
+      <div className="flex flex-col w-full px-8 sm:my-10 md:my-0 ">
         <div className="border-b-8 border-jobloop-primary-green md:pb-2 pt-2 mb-4">
           <h1 className="text-kv-black">{content.title}</h1>
         </div>
-        <div className="md:text-xl leading-relaxed tracking-wide">
+        <div className="text-jobloop-darker-green md:text-xl leading-relaxed tracking-wide">
           <PortableText value={content.content} />
         </div>
         <div className="flex justify-start mt-10">
