ccd7afa1ca49cc6930852c212d5d3885d9c02ce5
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-06-26 16:56:25 +0200

    update. Landingpage complete. Updated loads of containers and semantics.

--- components/LandingHero.jsx
+++ components/LandingHero.jsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 import Image from "next/image";
 import danialigdery from "../public/danialigdery.jpg";
 import { LinkButton, LinkButtonContained } from "./Buttons";
-import { PortableText } from "@portabletext/react";
+// import { PortableText } from "@portabletext/react";
 
 export default function LandingHero({ content }) {
   return (
     <>
-      <div className="relative h-[80vh] w-screen">
+      <div className="group relative h-[80vh] w-screen">
         <Image
           src={content.image}
           height={500}
@@ -17,9 +17,9 @@ export default function LandingHero({ content }) {
           alt="Scenic landscape"
           className="absolute inset-0 h-full w-full object-cover"
         />
-        <div className="absolute inset-0 flex flex-col items-center justify-center bg-black/50 p-6 text-center text-white">
+        <div className="absolute inset-0 flex flex-col items-center justify-center bg-black/60 backdrop-blur-sm p-6 text-center text-white">
           <div className="space-y-4">
-            <h1 className="text-6xl text-kv-white md:text-7xl lg:text-9xl">
+            <h1 className="text-6xl text-kv-white underline underline-offset-8 decoration-jobloop-primary-green group-hover:decoration-jobloop-primary-orange md:text-7xl lg:text-9xl">
               {content.title}
             </h1>
             <p className=" text-kv-white lg:text-3xl">
@@ -28,43 +28,37 @@ export default function LandingHero({ content }) {
           </div>
         </div>
       </div>
-      <div className="bg-kv-white bg-opacity-90 rounded-md backdrop-blur-md pt-4 px-4 pb-8 md:pb-0 flex flex-col justify-between w-full gap-32 md:flex-row">
-        <div className="flex justify-center px-4">
-          <div className="flex flex-col-reverse lg:flex-row gap-4 lg:gap-12 w-full">
-            <div className="flex flex-col w-full md:w-1/2 gap-10">
-              {/* <div className="text-lg lg:text-xl text-kv-black tracking-wide leading-relaxed pt-4">
+
+      <section className="flex flex-col-reverse justify-between py-12 px-4 lg:flex-row gap-4 md:gap-16 w-full">
+        {/* <div className="text-lg lg:text-xl text-kv-black tracking-wide leading-relaxed pt-4">
                  <PortableText value={content.content} /> 
               </div> */}
-              <div className="flex flex-col gap-4">
-                <h2 className="underline decoration-4 underline-offset-8 decoration-jobloop-primary-green">
-                  Hva gjør vi?
-                </h2>
-                <p className="text-lg md:text-3xl text-neutral-500">
-                  Vi rekrutterer kandidater som av ulike grunner har havnet
-                  utenfor arbeidslivet og lærer dem opp til å bli
-                  frontend-utviklere. Gjennom en kombinasjon av teoretisk
-                  opplæring og praksis vil kandidatene lære seg de viktigste
-                  teknologiene og bli klar for en karriere innen utvikling.
-                </p>
-              </div>
-              <div>
-                <LinkButtonContained
-                  Path={"/kontakt"}
-                  Text={"Kontakt oss"}
-                />
-              </div>
-            </div>
-            <div className="w-full lg:w-1/2">
-              <Image
-                width={800}
-                alt="kodemiljø"
-                src={danialigdery}
-                className="object-contain rounded"
-              />
-            </div>
-          </div>
+        <div className="w-full ">
+          <h2 className="underline decoration-4 pb-4 underline-offset-8 decoration-jobloop-primary-green">
+            Hva gjør vi?
+          </h2>
+          <p className="text-xl pb-12 leading-normal md:text-2xl md:leading-normal">
+            Vi rekrutterer kandidater som av ulike grunner har havnet utenfor
+            arbeidslivet og lærer dem opp til å bli frontend-utviklere. Gjennom
+            en kombinasjon av teoretisk opplæring og praksis vil kandidatene
+            lære seg de viktigste teknologiene og bli klar for en karriere innen
+            utvikling.
+          </p>
+          <LinkButtonContained
+            Path={"/kontakt"}
+            Text={"Kontakt oss"}
+          />
         </div>
-      </div>
+
+        <div className="w-full ">
+          <Image
+            width={800}
+            alt="kodemiljø"
+            src={danialigdery}
+            className="object-contain rounded"
+          />
+        </div>
+      </section>
     </>
   );
 }
--- components/LandingLinks.jsx
+++ components/LandingLinks.jsx
@@ -4,24 +4,24 @@ import dokument from "../public/dokument.svg";
 import koffert from "../public/koffert.svg";
 import hatt from "../public/hatt.svg";
 
-const LinkCard = ({ href, src, alt, title, description }) => {
+const LinkCard = ({ href, src, title, description }) => {
   return (
     <a
       href={href}
-      className="flex flex-col p-4 gap-2 sm:flex-row w-full md:flex-col md:align-start lg:flex-row md:p-6 shadow-md rounded-lg md:gap-8 hover:shadow-lg">
+      className="group bg-kv-white flex flex-col p-4 gap-2 sm:flex-row w-full md:flex-col md:align-start lg:flex-row md:p-6 shadow-md rounded-lg md:gap-8 hover:shadow-lg">
       <Image
         src={src}
-        width={500}
-        height={500}
-        className="h-20 w-16"
-        alt={alt}
+        width={64}
+        height={64}
+        className="stroke-2 stroke-jobloop-primary-green h-20 w-16 group-hover:rotate-3 transition-transform duration-300 ease-in-out"
+        alt={`Grafikkikon av ${src}`}
       />
-
+      {/* Text */}
       <div>
         <div className="w-full ">
-          <h2 className="text-xl font-bold hover:underline">{title}</h2>
+          <h2 className="text-xl font-bold ">{title}</h2>
           <p className="text-lg">{description}</p>
-          <div className="hover:underline text-blue-500">Les mer</div>
+          <div className="group-hover:underline text-blue-500">Les mer</div>
         </div>
       </div>
     </a>
@@ -30,34 +30,32 @@ const LinkCard = ({ href, src, alt, title, description }) => {
 
 export default function LandingLinks() {
   return (
-    <div className="flex flex-col justify-center gap-8 w-full px-8 pt-8">
-      <h2 className="text-center underline underline-offset-8 decoration-jobloop-primary-green">
-        Hvordan kan vi hjelpe deg?
-      </h2>
-
-      <div className="flex flex-col md:flex-row gap-8">
-        <LinkCard
-          href="/arbeidsgiver"
-          src={koffert}
-          alt=""
-          title="Arbeidsgiver"
-          description="For bedrifter som er interessert i utviklere"
-        />
-        <LinkCard
-          href="/deltaker"
-          src={hatt}
-          alt=""
-          title="Deltaker"
-          description="For deg som vil delta på Kodehode"
-        />
-        <LinkCard
-          href="/samarbeidspartner"
-          src={dokument}
-          alt=""
-          title="Samarbeidspartner"
-          description="For virksomheter som ønsker å samarbeide"
-        />
+    <section className="w-screen py-12">
+      <div className=" flex flex-col justify-center gap-8 max-w-screen-2xl mx-auto px-4">
+        <h2 className="text-center underline underline-offset-8 decoration-jobloop-primary-green">
+          Hvordan kan vi hjelpe deg?
+        </h2>
+        <div className="flex flex-col md:flex-row gap-8">
+          <LinkCard
+            href="/arbeidsgiver"
+            src={koffert}
+            title="Arbeidsgiver"
+            description="For bedrifter som er interessert i utviklere"
+          />
+          <LinkCard
+            href="/deltaker"
+            src={hatt}
+            title="Deltaker"
+            description="For deg som vil delta på Kodehode"
+          />
+          <LinkCard
+            href="/samarbeidspartner"
+            src={dokument}
+            title="Samarbeidspartner"
+            description="For virksomheter som ønsker å samarbeide"
+          />
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
