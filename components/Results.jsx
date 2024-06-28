01f1e52daa30f7efe86be6ebb4d0e0395d62b0ba
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-06-28 17:24:56 +0200

    Deltaker reverted back to design. updated code + clean-up

--- components/Results.jsx
+++ components/Results.jsx
@@ -7,33 +7,37 @@ import { PortableText } from "@portabletext/react";
 export default function Results({ content }) {
   //console.log(content);
   return (
-    <div className=" flex flex-col w-full gap-12 px-8 py-20 sm:px-10 md:px-12 lg:px-24 sm:flex-row 2xl:max-w-[1600px]">
-      <div className="w-full">
+    <section className="group w-screen py-12">
+      <div className=" flex flex-col md:flex-row justify-center gap-8 max-w-screen-2xl mx-auto px-4">
         <Image
           src={content.image}
           width={500}
           height={500}
           alt={content.alt}
-          className="w-full rounded"
+          className="w-full rounded-md shadow-md"
         />
-      </div>
-      <div className="flex flex-col gap-4 w-full">
-        <div className="border-b-2 pb-2 border-jobloop-primary-orange">
-          <h2>Kodehode gir resultater</h2>
+
+        <div className=" flex flex-col gap-8 w-full">
+          <h2 className="underline underline-offset-8 decoration-jobloop-primary-orange group-hover:decoration-jobloop-primary-green">
+            Kodehode gir resultater
+          </h2>
+
+          <div>
+            <div className="text-lg md:text-2xl pb-2 italic">
+              <PortableText value={content.content} />
+            </div>
+            <b> - {content.person}</b>
+          </div>
+          <a
+            href={content.link}
+            target="blank"
+            rel="noopener noreferrer"
+            aria-label="lenke til artikkelen"
+            className="text-blue-500 hover:text-blue-700 group-hover:font-bold hover:underline underline-offset-8">
+            Les mer på Kode24
+          </a>
         </div>
-        <div className="text-lg">
-          <PortableText value={content.content} />
-        </div>
-        <p className="py-5"> - {content.person}</p>
-        <a
-          href={content.link}
-          target="blank"
-          rel="noopener noreferrer"
-          aria-label="lenke til artikkelen"
-          className="underline text-jobloop-primary-green max-w-fit">
-          Les mer på Kode24
-        </a>
       </div>
-    </div>
+    </section>
   );
 }
