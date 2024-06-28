2039e9a01028093f49aeccaa5b60b5d718a615eb
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-06-28 13:32:19 +0200

    Fouricons updated and reverted back to how the design wants it. Optimized code. Implemented alerts.

--- components/StudyInParts.jsx
+++ components/StudyInParts.jsx
@@ -1,46 +1,49 @@
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
 import { PortableText } from "@portabletext/react";
 
 export default function StudyInParts({ content }) {
   return (
-    <div className="bg-neutral-100 mb-4 pt-8 md:pt-12">
-      <div className="flex flex-col px-8 md:flex-row  sm:px-10 md:px-12 lg:px-24 md:w-screen 2xl:justify-center">
-        <div className="flex flex-col pb-5 md:w-1/2 md:pb-0 2xl:max-w-[800px]">
-          <h2 className="tracking-tight">{content[0].title}</h2>
-          <div className="md:text-lg">
+    <section className="bg-neutral-50 w-screen py-12">
+      <div className="flex flex-col-reverse md:flex-row justify-center gap-8 max-w-screen-2xl mx-auto p-4">
+        <div className="w-full">
+          <h2 className="underline decoration-jobloop-primary-green underline-offset-8 pb-4">
+            {content[0].title}
+          </h2>
+          <div className="md:text-xl">
             <PortableText value={content[0].content} />
           </div>
         </div>
-        <div className="pl-0 my-auto md:w-1/2 md:pl-6 2xl:max-w-[800px]">
+        <div className="w-full">
           <Image
             src={content[0].image}
             width={500}
             height={500}
             alt={content[0].alt || ""}
-            className="mx-auto my-auto w-full rounded-md md:ml-auto md:mr-0"
+            className="rounded w-full"
           />
         </div>
       </div>
-      <div className="flex flex-col px-8 py-20 md:flex-row-reverse sm:px-10 md:px-12 lg:px-24 md:w-screen 2xl:justify-center">
-        <div className="flex flex-col pb-5 md:w-1/2 md:pb-0 2xl:max-w-[800px]">
-          <h2 className="tracking-tight">{content[1].title}</h2>
-          <div className="md:text-lg">
+      <div className="flex flex-col-reverse md:flex-row-reverse justify-center gap-8 max-w-screen-2xl mx-auto p-4">
+        <div className="w-full">
+          <h2 className="underline decoration-jobloop-primary-green underline-offset-8 pb-4">
+            {content[1].title}
+          </h2>
+          <div className="md:text-xl">
             <PortableText value={content[1].content} />
           </div>
         </div>
-        <div className="pr-0 my-auto md:w-1/2 md:pr-6 2xl:max-w-[800px]">
+        <div className="w-full">
           <Image
             src={content[1].image}
             width={500}
             height={500}
             alt={content[1].alt || ""}
-            className="mx-auto my-auto w-full rounded-md md:mr-0 md:ml-0"
+            className="rounded w-full"
           />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
