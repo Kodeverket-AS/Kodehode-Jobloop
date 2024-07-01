8192c254a51a79bca310e27d794fe9b4cdc51e9c
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-07-01 14:59:47 +0200

    Updated VisjonPage. Reformat ++

--- src/app/visjon/page.js
+++ src/app/visjon/page.js
@@ -1,10 +1,10 @@
 import { getData } from "../page";
-import EndorsementComp from "../../../components/EndorsementComp";
 import ContactComp from "../../../components/ContactComp";
-import Partners from "../../../components/PartnersComp";
-import { ProjectScroll } from "../../../components/ProjectScroll";
-import { CooptScroll } from "../../../components/ProjectScroll";
-import Vision2Part from "../../../components/Vision2Part";
+
+import {
+  VisionTextLeft,
+  VisionTextRight,
+} from "../../../components/Vision2Part";
 import VisionTop from "../../../components/VisionSub";
 
 export const metadata = {
@@ -16,35 +16,26 @@ export default async function Vision() {
 
   return (
     <>
-      <main className="flex flex-col items-center w-full min-h-screen">
-        <VisionTop
-          content={data.heroSub[3]}
-          key={data.heroSub[3]._id}
-        />
-        <div className="flex flex-col items-center justify-center md:mt-0">
-          <div className="w-full bg-neutral-100 pt-12 pb-16 flex flex-col gap-12">
-            {/* <div className="w-fit m-auto">
-              <h2 className="text-2xl font-extrabold text-kv-black md:text-3xl">
-                Samarbeidsprosjekter
-              </h2>
-              <hr className="w-full h-2 bg-jobloop-secondary-orange mt-2" />
-            </div> */}
-            <Vision2Part
-              content={data.VisionSections[1]}
-              link="https://glode.no/"
-              linkText="Mer om Gløde"
-              linkAria="Les mer om Gløde på deres nettside"
-              title="Gløde"
-            />
-            <Vision2Part
-              content={data.VisionSections[0]}
-              link="https://www.fontenehusetbergen.no/"
-              linkText="Mer om Fontenehuset"
-              linkAria="Les mer om Fontenehuset Bergen på deres nettside"
-              title="Fontenehuset Bergen"
-              imgSide="left"
-            />
-          </div>
+      <main className="flex flex-col items-center min-h-screen py-20">
+        <div className="flex flex-col gap-8 items-center ">
+          <VisionTop
+            content={data.heroSub[3]}
+            key={data.heroSub[3]._id}
+          />
+          <VisionTextRight
+            content={data.VisionSections[1]}
+            link="https://glode.no/"
+            linkText="Mer om Gløde"
+            linkAria="Les mer om Gløde på deres nettside"
+            title="Gløde"
+          />
+          <VisionTextLeft
+            content={data.VisionSections[0]}
+            link="https://www.fontenehusetbergen.no/"
+            linkText="Mer om Fontenehuset"
+            linkAria="Les mer om Fontenehuset Bergen på deres nettside"
+            title="Fontenehuset Bergen"
+          />
           {/* <div className="w-full">
             <div className="w-fit m-auto mt-8 md:mt-16">
               <h1 className="text-2xl font-extrabold text-center md:text-4xl ">
