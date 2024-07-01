8004a0917336b8fc71d0403ea9eb95f41a85db7f
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-07-01 12:35:29 +0200

    Updated kontakt.

--- components/ContactUsSection.jsx
+++ components/ContactUsSection.jsx
@@ -38,96 +38,99 @@ export function LocationCheck({ content }) {
   console.log(filters);
 
   return (
-    <div className="flex flex-col justify-center gap-4 max-w-[1600px]">
-      <div className="flex flex-col gap-2 pb-2">
-        <div className="border-b-2 pb-1">
-          <b>Velg sted :</b>
+    <section className="w-screen py-12">
+      <div className="flex flex-col  justify-center gap-12 max-w-screen-2xl mx-auto px-4">
+        <div className="flex flex-col gap-2 pb-2">
+          <div className="border-b-2 pb-1">
+            <b>Velg sted :</b>
+          </div>
+          {filterItems ? (
+            <FormControl className="flex flex-row gap-8 ">
+              <RadioGroup
+                value={selectedFilters}
+                onChange={handleFilterChange}>
+                <FormControlLabel
+                  value=""
+                  control={<Radio />}
+                  label="Alle byer"
+                  aria-label="Velg alle byene"
+                />
+                <div>
+                  {filters.map((location, idx) => (
+                    <FormControlLabel
+                      key={`filters-${idx}`}
+                      value={location}
+                      control={<Radio />}
+                      label={location}
+                      aria-label="Velg en av byene som er tilgjengelig."
+                    />
+                  ))}
+                </div>
+              </RadioGroup>
+            </FormControl>
+          ) : (
+            <b>Ingen ansatte funnet</b>
+          )}
         </div>
-        {filterItems ? (
-          <FormControl className="flex flex-row gap-8 ">
-            <RadioGroup
-              value={selectedFilters}
-              onChange={handleFilterChange}>
-              <FormControlLabel
-                value=""
-                control={<Radio />}
-                label="Alle byer"
-                aria-label="Velg alle byene"
-              />
-              <div>
-                {filters.map((location, idx) => (
-                  <FormControlLabel
-                    key={`filters-${idx}`}
-                    value={location}
-                    control={<Radio />}
-                    label={location}
-                    aria-label="Velg en av byene som er tilgjengelig."
-                  />
-                ))}
+        <div className="border-b-2 pb-1">
+          {filteredItems && selectedFilters !== "" ? (
+            <b>{`${filteredItems.length} personer i ${selectedFilters}`}</b>
+          ) : (
+            <b>{`${filteredItems.length} personer fordelt over ${filters.length} byer.`}</b>
+          )}
+        </div>
+        {filteredItems ? (
+          <div className="grid grid-cols-1 w-full gap-6 md:grid-cols-2 lg:grid-cols-3">
+            {filteredItems.map((item, idx) => (
+              <div
+                key={`items-${idx}`}
+                className="group flex flex-row h-56 w-full justify-top p-2 gap-3"
+                title={`Dette er ${item.navn}`}>
+                <div className="flex  h-full w-1/3 rounded-md overflow-hidden">
+                  {item.image ? (
+                    <Image
+                      width={500}
+                      height={500}
+                      className="w-full h-full object-cover object-center"
+                      alt={`Profilbilde ${item.navn}` || "profilbilder"}
+                      src={item.image || ""}
+                    />
+                  ) : (
+                    <div className="w-full h-full bg-gray-200 flex flex-col justify-center text-xs">
+                      <p className="text-center">
+                        <b>
+                          Bilde mangler av: <br />
+                        </b>{" "}
+                        {item.navn}
+                      </p>
+                    </div>
+                  )}
+                </div>
+                <div className="flex flex-col w-2/3">
+                  <div className="border-b-4 group-hover:border-jobloop-primary-green mb-1 w-full">
+                    <h3 className="text-xl">{item.navn}</h3>
+                  </div>
+                  <div>
+                    <b>Avdeling</b>
+                    <p>{item.lokasjon}</p>
+                  </div>
+                  <div>
+                    <b>E-post</b>
+                    <p>{item.mail}</p>
+                  </div>
+                  <div>
+                    <b>Telefon</b>
+                    <p>{item.tlf_nummer}</p>
+                  </div>
+                  {/* <p>Om:{item.om}</p> */}
+                </div>
               </div>
-            </RadioGroup>
-          </FormControl>
+            ))}
+          </div>
         ) : (
           <b>Ingen ansatte funnet</b>
         )}
       </div>
-
-      <div className="border-b-2 pb-1">
-        {filteredItems && selectedFilters !== "" ? (
-          <b>{`${filteredItems.length} personer i ${selectedFilters}`}</b>
-        ) : (
-          <b>{`${filteredItems.length} personer fordelt over ${filters.length} byer.`}</b>
-        )}
-      </div>
-      {filteredItems ? (
-        <div className="grid grid-cols-1 w-full gap-6 md:grid-cols-2 lg:grid-cols-3">
-          {filteredItems.map((item, idx) => (
-            <div
-              key={`items-${idx}`}
-              className="flex flex-col justify-top p-2 gap-2 rounded-md shadow-md">
-              <div className="flex h-72 rounded-sm overflow-hidden">
-                {item.image ? (
-                  <Image
-                    width={500}
-                    height={500}
-                    className="w-full h-full object-center object-cover"
-                    alt={`Profilbilde ${item.navn}` || "profilbilder"}
-                    src={item.image || ""}
-                  />
-                ) : (
-                  <div className="w-full h-full bg-gray-200 flex flex-col justify-center text-xs">
-                    <b className="text-center min-w-[350px]">
-                      Bilde mangler mangler av:
-                    </b>
-                    <p className="text-center">{item.navn}</p>
-                  </div>
-                )}
-              </div>
-              <div className="flex flex-col w-full gap-1">
-                <div className="border-b-4 border-jobloop-primary-green mb-1 w-full">
-                  <h3 className="text-xl">{item.navn}</h3>
-                </div>
-                <div className="pb-1">
-                  <b>Avdeling</b>
-                  <p>{item.lokasjon}</p>
-                </div>
-                <div className="pb-1">
-                  <b>E-post</b>
-                  <p>{item.mail}</p>
-                </div>
-                <div className="pb-1">
-                  <b>Telefon</b>
-                  <p>{item.tlf_nummer}</p>
-                </div>
-
-                <p>Om:{item.om}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <b>Ingen ansatte funnet</b>
-      )}
-    </div>
+    </section>
   );
 }
--- src/app/kontakt/page.js
+++ src/app/kontakt/page.js
@@ -12,12 +12,10 @@ export default async function Contact() {
 
   return (
     <div>
-      <main className="flex flex-col pt-10 mb-20 items-center min-h-screen ">
-        <div className="mb-16 ml-4 md:p-32 md:mb-0 ">
-          <div className="border-b-8 pb-2 border-jobloop-primary-green">
-            <h1 className="text-4xl text-black md:text-5xl">Kontakt oss</h1>
-          </div>
-        </div>
+      <main className="flex flex-col items-center min-h-screen py-20 md:py-40">
+        <h1 className="text-center underline decoration-4 pb-8 underline-offset-8 decoration-jobloop-primary-green">
+          Kontakt oss
+        </h1>
 
         <LocationCheck content={data.personDatabase}></LocationCheck>
       </main>
