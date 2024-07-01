d45df0de5d578c50020caf6797f98fa77f44a85d
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-07-01 15:03:57 +0200

    fixed so that img shows at top of text on mobile

--- components/Vision2Part.jsx
+++ components/Vision2Part.jsx
@@ -9,30 +9,28 @@ export function VisionTextRight({ content, link, linkText, linkAria, title }) {
       <div
         key={content._id}
         className="flex flex-col md:flex-row justify-center gap-12 max-w-screen-2xl mx-auto px-4">
-        <div className="flex justify-between gap-8">
-          <div className="w-full">
-            <Image
-              width={1000}
-              height={1000}
-              className="w-full"
-              src={content.ImageTwo}
-              alt={content.AltTwo || ""}
-            />
-          </div>
-          <div className="flex flex-col w-full gap-4">
-            <h2 className="text-lg xl:text-3xl underline underline-offset-8 pb-4 decoration-jobloop-primary-orange">
-              {title}
-            </h2>
-            <div>
-              <div className="pb-8 text-lg md:text-xl">
-                <PortableText value={content.content} />
-              </div>
-              <ExternalLinkButton
-                path={link}
-                text={linkText}
-                ariaLabel={linkAria}
-              />
+        <div className="w-full">
+          <Image
+            width={1000}
+            height={1000}
+            className="w-full"
+            src={content.ImageTwo}
+            alt={content.AltTwo || ""}
+          />
+        </div>
+        <div className="flex flex-col w-full gap-4">
+          <h2 className="text-lg xl:text-3xl underline underline-offset-8 pb-4 decoration-jobloop-primary-orange">
+            {title}
+          </h2>
+          <div>
+            <div className="pb-8 text-lg md:text-xl">
+              <PortableText value={content.content} />
             </div>
+            <ExternalLinkButton
+              path={link}
+              text={linkText}
+              ariaLabel={linkAria}
+            />
           </div>
         </div>
       </div>
@@ -46,32 +44,30 @@ export function VisionTextLeft({ content, link, linkText, linkAria, title }) {
     <section className="w-screen py-12">
       <div
         key={content._id}
-        className="flex flex-col md:flex-row justify-center gap-12 max-w-screen-2xl mx-auto px-4">
-        <div className="flex justify-between gap-8">
-          <div className="flex flex-col w-full gap-4">
-            <h2 className="text-lg xl:text-3xl underline underline-offset-8 pb-4 decoration-jobloop-primary-orange">
-              {title}
-            </h2>
-            <div>
-              <div className="pb-8 text-lg md:text-xl">
-                <PortableText value={content.content} />
-              </div>
-              <ExternalLinkButton
-                path={link}
-                text={linkText}
-                ariaLabel={linkAria}
-              />
+        className="flex flex-col-reverse md:flex-row justify-center gap-12 max-w-screen-2xl mx-auto px-4">
+        <div className="flex flex-col w-full gap-4">
+          <h2 className="text-lg xl:text-3xl underline underline-offset-8 pb-4 decoration-jobloop-primary-orange">
+            {title}
+          </h2>
+          <div>
+            <div className="pb-8 text-lg md:text-xl">
+              <PortableText value={content.content} />
             </div>
-          </div>
-          <div className="w-full">
-            <Image
-              width={1000}
-              height={1000}
-              className="w-full"
-              src={content.ImageTwo}
-              alt={content.AltTwo || ""}
+            <ExternalLinkButton
+              path={link}
+              text={linkText}
+              ariaLabel={linkAria}
             />
           </div>
+        </div>
+        <div className="w-full">
+          <Image
+            width={1000}
+            height={1000}
+            className="w-full"
+            src={content.ImageTwo}
+            alt={content.AltTwo || ""}
+          />
         </div>
       </div>
     </section>
