7b142e03f06229ae42cd343a06e1a5a3ef0d39ce
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-09-03 11:25:21 +0200

    udpated DynamicCompetence with grid so that it looks organized and aligned

--- components/DynamicCompetence.jsx
+++ components/DynamicCompetence.jsx
@@ -6,12 +6,12 @@ const LinkCard = ({ id, src, title, description }) => {
   return (
     <div
       key={id}
-      className="group transition-all duration-300 lg:max-w-md bg-kv-white flex flex-col p-4 gap-2 sm:flex-row w-full md:flex-col md:align-start lg:flex-row md:p-6 shadow-sm shadow-jobloop-primary-green rounded-lg md:gap-8 hover:shadow-lg">
+      className="group transition-all duration-300 bg-kv-white md:flex items-center p-2 gap-2 md:p-6 shadow-sm shadow-jobloop-primary-green rounded-lg md:gap-8 hover:shadow-lg">
       <Image
         src={src}
         width={64}
         height={64}
-        className="animate-pulse h-20 w-16 group-hover:rotate-3 transition-transform duration-300 ease-in-out"
+        className="animate-pulse h-16 w-16 group-hover:rotate-3 transition-transform duration-300 ease-in-out"
         alt={`Grafikkikon av ${src}`}
       />
       {/* Text */}
@@ -28,7 +28,7 @@ const LinkCard = ({ id, src, title, description }) => {
 export default function DynamicCompetence({ content }) {
   return (
     <div className="">
-      <div className="flex flex-wrap md:flex-row gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {content.map((item, index) => (
           <LinkCard
             key={index}
