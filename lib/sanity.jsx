0826579d2018852474a6007ef3ca6a10fa8eaef8
Andreastak86 <andreastak.kodehode@gmail.com>
2025-04-30 12:15:21 +0200

    update sanity.jsx file

--- lib/sanity.jsx
+++ lib/sanity.jsx
@@ -5,8 +5,9 @@ const dataset = process.env.NEXT_PUBLIC_DATASET;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
 
 export const Client = createClient({
-  projectId,
-  dataset,
-  apiVersion, // https://www.sanity.io/docs/api-versioning
-  useCdn: false, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
+    projectId,
+    dataset,
+    apiVersion, // https://www.sanity.io/docs/api-versioning
+    // useCdn: false,
+    // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
 });
